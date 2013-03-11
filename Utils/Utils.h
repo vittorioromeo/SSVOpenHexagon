5885eb4f2deaf24dc6970f02e2eb2ed81caffdd3
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-03-11 20:34:51 +0100

    1.82

--- Utils/Utils.h
+++ Utils/Utils.h
@@ -31,6 +31,7 @@ namespace hg
 	sf::Color getColorDarkened(sf::Color, float);
 	sf::Color getColorFromJsonArray(Json::Value mArray);
 
+	std::string get181FileContents(const std::string& mFilePath);
 	std::string getFileContents(const std::string& mFilePath);
 	Json::Value getJsonFileRoot(const std::string& mFilePath);
 	Json::Value getJsonFromString(const std::string& mString);
