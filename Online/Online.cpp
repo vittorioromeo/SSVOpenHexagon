bec44aa5fe4447379e647ad515792ede59f91270
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-03-12 15:26:45 +0100

    Getting started for 1.83

--- Online/Online.cpp
+++ Online/Online.cpp
@@ -28,7 +28,7 @@ namespace hg
 		using Status = Http::Response::Status;
 
 		const string host{"http://vittorioromeo.info"};
-		const string folder{"Misc/Linked/OHServer/"};
+		const string folder{"Misc/Linked/OHServer/dev/"};
 		const string infoFile{"OHInfo.json"};
 		const string scoresFile{"scores.json"};
 		const string sendScoreFile{"sendScore.php"};
