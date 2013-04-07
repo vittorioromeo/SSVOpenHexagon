0c71e31099313e9092914bf1a5e6cf94c642cb72
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-04-07 16:54:41 +0200

    Version text position

--- src/SSVOpenHexagon/Core/MenuGame.cpp
+++ src/SSVOpenHexagon/Core/MenuGame.cpp
@@ -52,7 +52,7 @@ namespace hg
 
 		versionText.setString(toStr(getVersion()));
 		versionText.setColor(Color::White);
-		versionText.setPosition(titleBar.getPosition() + Vector2f{titleBar.getGlobalBounds().width - 67, titleBar.getGlobalBounds().top});
+		versionText.setPosition(titleBar.getPosition() + Vector2f{titleBar.getGlobalBounds().width - 83, titleBar.getGlobalBounds().top});
 
 		creditsBar1.setOrigin({1024, 0});
 		creditsBar1.setScale({0.373f, 0.373f});
