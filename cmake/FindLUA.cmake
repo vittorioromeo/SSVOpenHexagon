c4371bd1c4d429e72452dbeef5f23d29a9f8fc6a
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-05-20 17:38:41 +0200

    submodule update

--- cmake/FindLUA.cmake
+++ cmake/FindLUA.cmake
@@ -1,23 +1,15 @@
-# -*- cmake -*-
-
-# - Find LUA
-# Find the LUA includes and library
-# This module defines
-#  LUA_INCLUDE_DIR, where to find lua.hpp
-#  LUA_LIBRARIES, the libraries needed to use LUA.
-#  LUA_FOUND, If false, do not try to use LUA.
-#  also defined, but not for general use are
-#  LUA_LIBRARY, where to find the LUA library.
-
 FIND_PATH(LUA_INCLUDE_DIR lua5.1/lua.hpp
   PATHS "${PROJECT_SOURCE_DIR}/../lua/"
+  PATH_SUFFIXES include/ lua/ lua/include/ ./
   "${PROJECT_SOURCE_DIR}/extlibs/lua/"
   ${LUA_ROOT}
   $ENV{LUA_ROOT}
-  /usr/local/include
-  /usr/include
-  /usr/include/lua
-  /usr/local/include/lua
+  /usr/local/
+  /usr/
+  /sw/
+  /opt/local/
+  /opt/csw/
+  /opt/
 )
 
 message("\nFound Lua include at: ${LUA_INCLUDE_DIR}.\n")
@@ -25,35 +17,35 @@ message("\nFound Lua include at: ${LUA_INCLUDE_DIR}.\n")
 FIND_LIBRARY(LUA_LIBRARY
   NAMES lua5.1.lib liblua5.1 lua5.1 lua liblua
   PATHS "${PROJECT_SOURCE_DIR}/../lua/"
+  PATH_SUFFIXES lib/ lib/x86_64-linux-gnu/ lib64/ lua/ lua/lib/ lua/lib64/ ./
   "${PROJECT_SOURCE_DIR}/extlibs/lua/"
   ${LUA_ROOT}
   $ENV{LUA_ROOT}
-  /usr/lib 
-  /usr/local/lib 
-  /lib 
-  /lib64
+  /usr/local/
+  /usr/
+  /sw/
+  /opt/local/
+  /opt/csw/
+  /opt/
+  /usr/lib/x86_64-linux-gnu
 )
 
 message("\nFound Lua library at: ${LUA_LIBRARY}.\n")
 
-IF (LUA_LIBRARY AND LUA_INCLUDE_DIR)
-    SET(LUA_LIBRARIES ${LUA_LIBRARY})
-    SET(LUA_FOUND "YES")
-ELSE (LUA_LIBRARY AND LUA_INCLUDE_DIR)
-  SET(LUA_FOUND "NO")
-ENDIF (LUA_LIBRARY AND LUA_INCLUDE_DIR)
+IF(LUA_LIBRARY AND LUA_INCLUDE_DIR)
+  SET(LUA_LIBRARIES ${LUA_LIBRARY})
+  SET(LUA_FOUND TRUE)
+ELSE(LUA_LIBRARY AND LUA_INCLUDE_DIR)
+  SET(LUA_FOUND FALSE)
+ENDIF(LUA_LIBRARY AND LUA_INCLUDE_DIR)
 
-IF (LUA_FOUND)
-      MESSAGE(STATUS "Found LUA in ${LUA_INCLUDE_DIR}")
-ELSE (LUA_FOUND)
-   IF (LUA_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find LUA library")
-   ENDIF (LUA_FIND_REQUIRED)
-ENDIF (LUA_FOUND)
-
-# Deprecated declarations.
-SET (NATIVE_LUA_INCLUDE_PATH ${LUA_INCLUDE_DIR} )
-GET_FILENAME_COMPONENT (NATIVE_LUA_LIB_PATH ${LUA_LIBRARY} PATH)
+IF(LUA_FOUND)
+  MESSAGE(STATUS "Found LUA in ${LUA_INCLUDE_DIR}")
+ELSE(LUA_FOUND)
+  IF(LUA_FIND_REQUIRED)
+  MESSAGE(FATAL_ERROR "Could not find LUA library")
+  ENDIF(LUA_FIND_REQUIRED)
+ENDIF(LUA_FOUND)
 
 MARK_AS_ADVANCED(
   LUA_LIBRARY
