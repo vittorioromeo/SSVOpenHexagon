9b4a5eddaba6b6e9256bfbdcee34e6207adbf2a4
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-07-10 00:26:00 +0200

    build-repository-rel script is now decent!

--- build-repository-rel.sh
+++ build-repository-rel.sh
@@ -4,29 +4,42 @@
 # RELEASE MODE, SKIPS RPATH
 # Takes $1 destination directory parameter
 
-PROJECTNAME=${PWD##*/} # Project to build (current directory name)
-BUILDTYPE="RELEASE" # Passed to CMake (CMAKE_BUILD_TYPE)
-BUILDSHARED="TRUE" # Passed to CMake (LIBNAME_BUILD_SHARED_LIB)
-MAKEJOBS="4" # make -j...
+if [ -z "${1}" ]; then
+	echo "This script the installation path as an argument!"
+	exit 1
+fi
 
-CMAKEFLAGS="-DCMAKE_BUILD_TYPE=$BUILDTYPE -DCMAKE_SKIP_BUILD_RPATH=TRUE"
+absolutePath=$(readlink -f ${1})
 
-export DESTDIR="$1/"
-export CMAKE_PREFIX_PATH="$1/"
+projectName="SSVOpenHexagon"
+buildType="Release"
+buildSharedLibs="True"
+makeJobs="8"
+destinationDir="${absolutePath}/"
+cmakePrefixPath="${absolutePath}/temp"
+cmakeFlags="-DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_SKIP_BUILD_RPATH=True"
 
-echo "building for release"
-echo "CMAKEFLAGS = $CMAKEFLAGS"
-echo "DESTDIR = $DESTDIR"
-echo "CMAKE_PREFIX_PATH = $CMAKE_PREFIX_PATH"
+if [ ! -d "${destinationDir}" ]; then
+  	echo "${destinationDir} does not exist!"
+  	exit 1
+fi
 
-read -p "Continue? " -n 1
+echo "Building a release version of ${projectName}"
+echo "cmakeFlags: ${cmakeFlags}"
+echo "cmakePrefixPath: ${cmakePrefixPath}"
+echo "destinationDir: ${destinationDir}"
+echo "All files in ${destinationDir} will be removed"
+
+read -p "Continue? [Y/N] " -n 1
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
 fi
 
-rm -Rf "$1/"*
+rm -Rf "${destinationDir}"*
 
-LIBS=("SSVJsonCpp" "SSVUtils" "SSVUtilsJson" "SSVMenuSystem" "SSVEntitySystem" "SSVLuaWrapper" "SSVStart") # List of extlibs to build in order
+dependencies=("SSVJsonCpp" "SSVUtils" "SSVUtilsJson" "SSVMenuSystem" "SSVEntitySystem" "SSVLuaWrapper" "SSVStart") # List of extlibs to build in order
+
+export DESTDIR="${cmakePrefixPath}"
 
 function warn() {
 	echo "Error occured in: `pwd`"; echo "Error was: "$@
@@ -39,97 +52,73 @@ function die() {
 # Builds a lib, with name $1 - calls CMake, make -j and make install -j
 function buildLib
 {
-	local LIBNAME="$1"
+	local libName="${1}"
 
-	echo "Building $LIBNAME..."
-  	cd $LIBNAME # Enter lib main directory (where CMakeLists.txt is)
-  	mkdir build; cd build # Create and move to the build directory
+	echo "Building ${libName}..."
+  	cd "${libName}" # Enter lib main directory (where CMakeLists.txt is)
+  	mkdir "build"; cd "build" # Create and move to the build directory
 
 	# Run CMake, make and make install
-	cmake ../ -DBUILD_SHARED_LIB=$BUILDSHARED $CMAKEFLAGS || \
-		die 1 "cmake failed"
+	cmake ../ "-DBUILD_SHARED_LIB=${buildSharedLibs} ${cmakeFlags}" || die 1 "cmake failed"
 
-	make -j$MAKEJOBS || \
-		die 1 "make failed"
-
-	make install -j$MAKEJOBS || \
-		die 1 "make install failed"
+	make "-j${makeJobs}" || die 1 "make failed"
+	make install "-j${makeJobs}" || die 1 "make install failed"
 
 	cd ../.. # Go back to extlibs directory
-	echo "Finished building $LIBNAME..."
+	echo "Finished building ${libName}..."
 }
 
-cd extlibs  # Start building... Enter extlibs, and build extlibs
-for LIB in ${LIBS[*]}; do buildLib $LIB; done
-cd .. # Now we are in the main folder
-echo "Building $PROJECTNAME..."
-mkdir build; cd build # Create and move to the build directory
-
+cd "extlibs"  # Start building... Enter extlibs, and build extlibs
+for l in "${dependencies[@]}"; do buildLib "${l}"; done
+cd ".." # Now we are in the main folder
+echo "Building ${projectName}..."
+mkdir "build"; cd "build" # Create and move to the build directory
 
 ## Run CMake, make and make install
-cmake ../ $CMAKEFLAGS
-make -j$MAKEJOBS; make install -j$MAKEJOBS
+cmake ../ "${cmakeFlags}"
+make "-j${makeJobs}"; make install "-j${makeJobs}"
 
-cd ..
-echo "Finished building $PROJECTNAME..."
-echo "copying libraries"
+cd ".."
+echo "Finished building ${projectName}..."
 
-DESTLIBPATH="${CMAKE_PREFIX_PATH}games/SSVOpenHexagon/x86/"
-mkdir "${DESTLIBPATH}"
+mv "${cmakePrefixPath}/usr/local/games/SSVOpenHexagon/"* "${destinationDir}"
 
-echo $DESTLIBPATH
-echo "${CMAKE_PREFIX_PATH}lib/*"
+echo "Copying system libraries..."
 
-cp -av "${CMAKE_PREFIX_PATH}lib/"* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"libsfml*[!d].so* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"libfreetype*.so* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"libjpeg*.so* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"liblua*.so* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"libGLEW*.so* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"libopenal*.so* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"libstdc++*.so* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"libsfml*.so* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"libsndfile*.so* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"libFLAC*.so* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"libogg.so* "$DESTLIBPATH"
-cp -av "/usr/local/lib/"libvorbis*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/"libsfml*[!d].so* "$DESTLIBPATH"
-cp -av "/usr/lib/"libfreetype*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/"libjpeg*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/"liblua*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/"libGLEW*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/"libopenal*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/"libstdc++*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/"libsndfile*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/"libFLAC*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/"libogg.so* "$DESTLIBPATH"
-cp -av "/usr/lib/"libvorbis*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/i386-linux-gnu/"libsfml*[!d].so* "$DESTLIBPATH"
-cp -av "/usr/lib/i386-linux-gnu/"libfreetype*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/i386-linux-gnu/"libjpeg*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/i386-linux-gnu/"liblua*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/i386-linux-gnu/"libGLEW*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/i386-linux-gnu/"libopenal*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/i386-linux-gnu/"libstdc++*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/i386-linux-gnu/"libsndfile*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/i386-linux-gnu/"libFLAC*.so* "$DESTLIBPATH"
-cp -av "/usr/lib/i386-linux-gnu/"libogg.so* "$DESTLIBPATH"
-cp -av "/usr/lib/i386-linux-gnu/"libvorbis*.so* "$DESTLIBPATH"
+x86Folder="${destinationDir}/x86/"
+mkdir "${x86Folder}"
 
-mv -f "${CMAKE_PREFIX_PATH}games/SSVOpenHexagon/SSVOpenHexagon" "${CMAKE_PREFIX_PATH}games/SSVOpenHexagon/x86"
-chmod 777 "${CMAKE_PREFIX_PATH}games/SSVOpenHexagon/x86/SSVOpenHexagon"
+cp -av "${cmakePrefixPath}/lib/"* "${x86Folder}"
+echo "Removing temp directories..."
+rm -Rf "${cmakePrefixPath}"
 
-touch "${CMAKE_PREFIX_PATH}games/SSVOpenHexagon/OpenHexagon"
-chmod 777 "${CMAKE_PREFIX_PATH}games/SSVOpenHexagon/OpenHexagon"
+searchPaths=("/usr/local/lib/" "/usr/lib/" "/usr/lib/i386-linux-gnu/")
 
-echo '#!/bin/bash' > "${CMAKE_PREFIX_PATH}games/SSVOpenHexagon/OpenHexagon"
-echo 'export LD_LIBRARY_PATH=./x86/; ./x86/SSVOpenHexagon' >> "${CMAKE_PREFIX_PATH}games/SSVOpenHexagon/OpenHexagon"
+for s in "${searchPaths[@]}"; do
+	cp -av "${s}"libsfml*[!d].so* "${x86Folder}"
+	cp -av "${s}"libfreetype*.so* "${x86Folder}"
+	cp -av "${s}"libjpeg*.so* "${x86Folder}"
+	cp -av "${s}"liblua*.so* "${x86Folder}"
+	cp -av "${s}"libGLEW*.so* "${x86Folder}"
+	cp -av "${s}"libopenal*.so* "${x86Folder}"
+	cp -av "${s}"libstdc++*.so* "${x86Folder}"
+	cp -av "${s}"libsfml*.so* "${x86Folder}"
+	cp -av "${s}"libsndfile*.so* "${x86Folder}"
+	cp -av "${s}"libFLAC*.so* "${x86Folder}"
+	cp -av "${s}"libogg.so* "${x86Folder}"
+	cp -av "${s}"libvorbis*.so* "${x86Folder}"
+done
 
-chmod a+x "${CMAKE_PREFIX_PATH}games/SSVOpenHexagon/OpenHexagon"
-mv -f "${CMAKE_PREFIX_PATH}games/SSVOpenHexagon/OpenHexagon" "${CMAKE_PREFIX_PATH}games/SSVOpenHexagon/OpenHexagon.sh"
+mv -f "${destinationDir}/SSVOpenHexagon" "${x86Folder}"
+chmod +x "${x86Folder}/SSVOpenHexagon"
 
-mv -f "${CMAKE_PREFIX_PATH}games/SSVOpenHexagon" "$1/"
-rm "$1/"[!S]* -Rf
+touch "${destinationDir}/OpenHexagon"
+chmod +x "${destinationDir}/OpenHexagon"
 
-find "$1" -name *'.so' | xargs strip -s -g
-find "$1" -name 'SSVOpenHexagon'* | xargs strip -s -g
+echo '#!/bin/bash' > "${destinationDir}/OpenHexagon"
+echo 'export LD_LIBRARY_PATH=./x86/; ./x86/SSVOpenHexagon' >> "${destinationDir}/OpenHexagon"
+
+find "${destinationDir}" -name *'.so' | xargs strip -s -g
+find "${destinationDir}" -name 'SSVOpenHexagon'* | xargs strip -s -g
+
+echo "Successfully finished."
