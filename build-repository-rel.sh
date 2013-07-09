161742755567f49af65291769f93bfb434314669
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-07-09 15:07:02 +0200

    updated scripts

--- build-repository-rel.sh
+++ build-repository-rel.sh
@@ -67,8 +67,6 @@ for LIB in ${LIBS[*]}; do buildLib $LIB; done
 cd .. # Now we are in the main folder
 echo "Building $PROJECTNAME..."
 rm CMakeCache.txt # Remove CMakeCache.txt, in case an earlier (accidental) build was made in the main directory
-mkdir build; cd build # Create and move to the build directory
-rm CMakeCache.txt # If the library was previously built, remove CMakeCache.txt
 
 ## Run CMake, make and make install
 cmake ../ $CMAKEFLAGS
