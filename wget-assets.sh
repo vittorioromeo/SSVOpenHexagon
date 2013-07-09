3dea308f41a0503467baa316096c43836ffb1ef1
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-07-09 14:51:16 +0200

    added asset downloader

--- wget-assets.sh
+++ wget-assets.sh
@@ -4,5 +4,5 @@
 
 FOLDERNAME="Testing"
 
-cd ./RELEASE
-wget -r --no-parent "http://vittorioromeo.info/Misc/Linked/OHResources/$FOLDERNAME"
+cd ./_RELEASE
+wget -r -nH --cut-dirs=4 --no-parent -e robots=off --reject="index.html*" "http://vittorioromeo.info/Misc/Linked/OHResources/$FOLDERNAME"
