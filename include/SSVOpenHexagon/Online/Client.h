bb9da4f9207a85398cb44a063b759e0020328a70
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-07-30 09:38:03 +0200

    Added missing copyrights

--- include/SSVOpenHexagon/Online/Client.h
+++ include/SSVOpenHexagon/Online/Client.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2013 Vittorio Romeo
+// License: Academic Free License ("AFL") v. 3.0
+// AFL License page: http://opensource.org/licenses/AFL-3.0
+
 #ifndef HG_ONLINE_CLIENT
 #define HG_ONLINE_CLIENT
 
