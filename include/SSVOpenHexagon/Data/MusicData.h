1b4d66f0c77f2f7682ccab68bee386b7d7723ed6
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-08-26 22:00:03 +0200

    Adapted to newest libs

--- include/SSVOpenHexagon/Data/MusicData.h
+++ include/SSVOpenHexagon/Data/MusicData.h
@@ -18,7 +18,7 @@ namespace hg
 		private:
 			std::vector<int> segments;
 
-			inline int getRandomSegment() const { return segments[ssvu::getRnd(0, segments.size())]; }
+			inline int getRandomSegment() const { return segments[ssvu::getRnd(0ul, segments.size())]; }
 
 		public:
 			std::string id, fileName, name, album, author;
