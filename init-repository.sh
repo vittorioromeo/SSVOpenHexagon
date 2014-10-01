f04e4e9b008967c6e432f2975459b0c4b26f9798
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-10-01 18:45:55 +0200

    init-repo

--- init-repository.sh
+++ init-repository.sh
@@ -5,8 +5,8 @@
 echo "Initializing all submodules..."
 git submodule update --init --recursive
 
-echo "Stashing all submodule changes..."
-git submodule foreach --recursive git stash
+echo "Discarding all submodule changes..."
+git submodule foreach --recursive "git fetch --all; git reset --hard origin/master"
 
 echo "Recursively pulling all submodules..."
 git submodule foreach --recursive git pull origin master --recurse-submodules
