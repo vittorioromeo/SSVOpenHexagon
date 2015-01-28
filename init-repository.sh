157818eb5ef950d9eb0ea2feeb2bebf9fcd30d8a
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-01-28 22:27:32 +0100

    script update

--- init-repository.sh
+++ init-repository.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# This bash script, called in a repository with submodules, inits and pulls all submodules recursively
+# This bash script, called in a repository with submodules, inits and pulls all submodules
 
 echo "Initializing all submodules..."
-git submodule update --init --recursive
+git submodule update --init
 
 echo "Discarding all submodule changes..."
-git submodule foreach --recursive "git fetch --all; git reset --hard origin/master"
+git submodule foreach "git fetch --depth=1 --all; git reset --hard origin/master"
 
 echo "Recursively pulling all submodules..."
-git submodule foreach --recursive git pull origin master --recurse-submodules
+git submodule foreach git pull --depth=1 origin master
