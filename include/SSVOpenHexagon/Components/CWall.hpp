920d38d6af215f9a2a896a689eeb6544604c11d3
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-02-19 18:45:54 +0100

    copyright

--- include/SSVOpenHexagon/Components/CWall.hpp
+++ include/SSVOpenHexagon/Components/CWall.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2014 Vittorio Romeo
+// Copyright (c) 2013-2015 Vittorio Romeo
 // License: Academic Free License ("AFL") v. 3.0
 // AFL License page: http://opensource.org/licenses/AFL-3.0
 
