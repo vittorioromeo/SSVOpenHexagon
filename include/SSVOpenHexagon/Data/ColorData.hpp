92afabe369c370d34e0f839968024302c55d2d03
Synth Morxemplum <morxemplum@outlook.com>
2020-06-20 12:10:33 -0700

    Fixed compiler errors

--- include/SSVOpenHexagon/Data/ColorData.hpp
+++ include/SSVOpenHexagon/Data/ColorData.hpp
@@ -2,6 +2,8 @@
 // License: Academic Free License ("AFL") v. 3.0
 // AFL License page: http://opensource.org/licenses/AFL-3.0
 
+#pragma once
+
 #include "SSVOpenHexagon/Global/Common.hpp"
 
 struct ColorData
