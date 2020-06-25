eb84dca8972004febb2c998bbf9220568aaa131e
Wolf Clement <c.wolf@kiwec.net>
2020-06-25 18:45:31 +0200

    Fix avengers level threat
    Happened when you paused exactly when the timer unpaused.

--- src/SSVOpenHexagon/Core/HGStatus.cpp
+++ src/SSVOpenHexagon/Core/HGStatus.cpp
@@ -2,50 +2,77 @@
 
 using namespace hg;
 
-float HexagonGameStatus::getIncrementTimeSeconds() {
-    if(isTimePaused()) {
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(pauseTp - incrementTp);
+float HexagonGameStatus::getIncrementTimeSeconds()
+{
+    if(isTimePaused())
+    {
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+            pauseTp - incrementTp);
         return static_cast<float>(ms.count()) / 1000.f;
-    } else {
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(lastTp - incrementTp);
+    }
+    else
+    {
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+            lastTp - incrementTp);
         return static_cast<float>(ms.count()) / 1000.f;
     }
 }
 
-float HexagonGameStatus::getTimeSeconds() {
-    if(isTimePaused()) {
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(pauseTp - startTp);
+float HexagonGameStatus::getTimeSeconds()
+{
+    if(isTimePaused())
+    {
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+            pauseTp - startTp);
         return static_cast<float>(ms.count()) / 1000.f;
-    } else {
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(lastTp - startTp);
+    }
+    else
+    {
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+            lastTp - startTp);
         return static_cast<float>(ms.count()) / 1000.f;
     }
 }
 
-bool HexagonGameStatus::isTimePaused() {
+bool HexagonGameStatus::isTimePaused()
+{
     return pauseDuration > (lastTp - pauseTp);
 }
 
-void HexagonGameStatus::pauseTime(float seconds) {
-    if(!isTimePaused()) {
-        pauseTp = lastTp;
+void HexagonGameStatus::pauseTime(float seconds)
+{
+    if(isTimePaused())
+    {
+        pauseDuration +=
+            std::chrono::milliseconds(static_cast<int>(seconds * 1000.f));
     }
-    pauseDuration = std::chrono::milliseconds(static_cast<int>(seconds * 1000.f));
+    else
+    {
+        pauseTp = lastTp;
+        pauseDuration =
+            std::chrono::milliseconds(static_cast<int>(seconds * 1000.f));
+    }
 }
 
-void HexagonGameStatus::resetIncrementTime() {
-    if(isTimePaused()) {
+void HexagonGameStatus::resetIncrementTime()
+{
+    if(isTimePaused())
+    {
         incrementTp = pauseTp;
-    } else {
+    }
+    else
+    {
         incrementTp = lastTp;
     }
 }
 
-void HexagonGameStatus::updateTime() {
+void HexagonGameStatus::updateTime()
+{
     bool wasPaused = isTimePaused();
 
     lastTp = std::chrono::steady_clock::now();
-    if(wasPaused && !isTimePaused()) {
+    if(wasPaused && !isTimePaused())
+    {
         startTp += pauseDuration;
         pauseTp = startTp;
         pauseDuration = 0ms;
