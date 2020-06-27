a68d866a47fbcd70219d42f743d36130b13521b2
Synth Morxemplum <morxemplum@outlook.com>
2020-06-27 10:24:10 -0700

    Speed caps now factor in difficultyMult

--- src/SSVOpenHexagon/Core/HexagonGame.cpp
+++ src/SSVOpenHexagon/Core/HexagonGame.cpp
@@ -209,9 +209,9 @@ void HexagonGame::sideChange(unsigned int mSideNumber)
     levelStatus.speedMult += levelStatus.speedInc;
     levelStatus.delayMult += levelStatus.delayInc;
 
-    // Cap speed at speed max. If speed max is zero, disregard the cap.
+    // Cap speed at speed max (with difficulty in mind). If speed max is zero, disregard the cap.
     const auto& speedMax(levelStatus.speedMax);
-    if (levelStatus.speedMult > speedMax && speedMax > 0.f)
+    if (levelStatus.speedMult * difficultyMult > speedMax && speedMax > 0.f)
     {
         levelStatus.speedMult = speedMax;
     }
