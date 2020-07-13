59b58b2da6ae53aa0f54d87545c4e1861e6ce302
Vittorio Romeo <vittorio.romeo@outlook.com>
2020-07-13 16:38:38 +0100

    Steam Build 2.01

--- prepare_release.sh
+++ prepare_release.sh
@@ -22,6 +22,7 @@ cp ./_RELEASE/sfml-network-2.dll ./_PREPARED_RELEASE
 cp ./_RELEASE/sfml-system-2.dll ./_PREPARED_RELEASE
 cp ./_RELEASE/sfml-window-2.dll ./_PREPARED_RELEASE
 cp ./_RELEASE/steam_api64.dll ./_PREPARED_RELEASE
+cp ./_RELEASE/discord_game_sdk.dll ./_PREPARED_RELEASE
 cp ./misc/default_config.json ./_PREPARED_RELEASE/config.json
 cp ./_RELEASE/steam_appid.txt ./_PREPARED_RELEASE
 cp ./_RELEASE/windowed.bat ./_PREPARED_RELEASE
