5005dae1a26c2dc8e92f5e8a1ebf4d6e37b83518
Vittorio Romeo <vittorio.romeo@outlook.com>
2020-07-16 22:12:01 +0100

    Fixup Zly's changes

--- src/SSVOpenHexagon/Core/MenuGame.cpp
+++ src/SSVOpenHexagon/Core/MenuGame.cpp
@@ -698,9 +698,9 @@ void MenuGame::initLua(Lua::LuaContext& mLua)
             "e_eventWaitS", "e_eventWaitUntilS", "w_wall", "w_wallAdj",
             "w_wallAcc", "w_wallHModSpeedData", "w_wallHModCurveData",
             "l_setDelayMult", "s_setStyle", "u_setMusic", "l_getRotation",
-            "l_setRotation", "s_getCameraShake", "s_setCameraShake", "s_setColorOffset",
-            "l_getOfficial", "steam_unlockAchievement", "cw_create",
-            "cw_destroy", "cw_setVertexPos", "cw_setVertexColor",
+            "l_setRotation", "s_getCameraShake", "s_setCameraShake",
+            "s_setColorOffset", "l_getOfficial", "steam_unlockAchievement",
+            "cw_create", "cw_destroy", "cw_setVertexPos", "cw_setVertexColor",
             "cw_isOverlappingPlayer", "cw_clear"})
     {
         mLua.writeVariable(un, [] {});
