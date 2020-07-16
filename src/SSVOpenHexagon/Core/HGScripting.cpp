dd8b347989d23c49d1e1b56be247f987b34f0bc5
Vittorio Romeo <vittorio.romeo@outlook.com>
2020-07-16 22:12:01 +0100

    Fixup Zly's changes

--- src/SSVOpenHexagon/Core/HGScripting.cpp
+++ src/SSVOpenHexagon/Core/HGScripting.cpp
@@ -449,11 +449,7 @@ void HexagonGame::initLua_StyleControl()
     sdVar("3dPulseMin", &StyleData::_3dPulseMin);
     sdVar("3dPulseSpeed", &StyleData::_3dPulseSpeed);
     sdVar("3dPerspectiveMult", &StyleData::_3dPerspectiveMult);
-
-    addLuaFn("s_setColorOffset",
-        [this](int mValue) {styleData.BGColorOffset = mValue;})
-        .arg("value")
-        .doc("Offsets the colors of the background tiles by `$0`.");
+    sdVar("BGColorOffset", &StyleData::BGColorOffset);
 
     addLuaFn("s_setStyle", //
         [this](std::string mId) {
@@ -494,7 +490,6 @@ void HexagonGame::initLua_StyleControl()
         .doc(
             "Set the color of the center polygon to match the  style "
             "color with index `$0`.");
-
 }
 
 void HexagonGame::initLua_WallCreation()
