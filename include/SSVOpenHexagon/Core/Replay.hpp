1032560f9ff7ab32e8e8fee97b77d6af9635665c
Vittorio Romeo <vittorio.romeo@outlook.com>
2020-07-24 20:07:37 +0100

    Implement replay serialization and replay file data structure

--- include/SSVOpenHexagon/Core/Replay.hpp
+++ include/SSVOpenHexagon/Core/Replay.hpp
@@ -9,6 +9,9 @@
 #include <bitset>
 #include <vector>
 #include <cstddef>
+#include <cstring>
+#include <cstdint>
+#include <string>
 
 namespace hg
 {
@@ -23,26 +26,67 @@ enum class input_bit : unsigned int
     k_count
 };
 
+// TODO: optimize size - `sizeof` this is `4`
 using input_bitset = std::bitset<static_cast<unsigned int>(input_bit::k_count)>;
+
+struct serialization_result
+{
+    std::size_t _written_bytes{0};
+    bool _success{true};
+
+    [[nodiscard]] explicit operator bool() const noexcept
+    {
+        return _success;
+    }
+
+    [[nodiscard]] std::size_t written_bytes() const noexcept
+    {
+        return _written_bytes;
+    }
+};
+
+struct deserialization_result
+{
+    std::size_t _read_bytes{0};
+    bool _success{true};
+
+    [[nodiscard]] explicit operator bool() const noexcept
+    {
+        return _success;
+    }
+
+    [[nodiscard]] std::size_t read_bytes() const noexcept
+    {
+        return _read_bytes;
+    }
+};
 
 class replay_data
 {
-public:
-    using seed_type = random_number_generator::seed_type;
-
 private:
-    seed_type _seed;
     std::vector<input_bitset> _inputs;
 
 public:
-    explicit replay_data(const seed_type seed) noexcept;
-
     void record_input(const bool left, const bool right, const bool swap,
         const bool focus) noexcept;
 
-    [[nodiscard]] seed_type get_seed() const noexcept;
     [[nodiscard]] input_bitset at(const std::size_t index) const noexcept;
     [[nodiscard]] std::size_t size() const noexcept;
+
+    [[nodiscard]] bool operator==(const replay_data& rhs) const noexcept;
+    [[nodiscard]] bool operator!=(const replay_data& rhs) const noexcept;
+
+    [[nodiscard]] serialization_result serialize(
+        std::byte* buffer, const std::size_t buffer_size);
+
+    [[nodiscard]] deserialization_result deserialize(
+        const std::byte* buffer, const std::size_t buffer_size);
+
+    [[nodiscard]] serialization_result serialize(
+        std::byte* buffer, const std::byte* const buffer_end);
+
+    [[nodiscard]] deserialization_result deserialize(
+        const std::byte* buffer, const std::byte* const buffer_end);
 };
 
 class replay_player
@@ -55,6 +99,36 @@ public:
     explicit replay_player(const replay_data& rd) noexcept;
 
     [[nodiscard]] input_bitset get_current_and_move_forward() noexcept;
+    [[nodiscard]] bool done() const noexcept;
+};
+
+struct replay_file
+{
+    using seed_type = random_number_generator::seed_type;
+
+    std::uint32_t _version;   // Replay format version.
+    std::string _player_name; // Name of the player.
+    seed_type _seed;          // RNG seed for the session.
+    replay_data _data;        // Input data.
+    std::string _pack_id;     // Id of the selected pack.
+    std::string _level_id;    // Id of the played level.
+    float _difficulty_mult;   // Played difficulty multiplier.
+    double _played_frametime; // Played frametime (excluding pauses).
+
+    [[nodiscard]] bool operator==(const replay_file& rhs) const noexcept;
+    [[nodiscard]] bool operator!=(const replay_file& rhs) const noexcept;
+
+    [[nodiscard]] serialization_result serialize(
+        std::byte* buffer, const std::size_t buffer_size);
+
+    [[nodiscard]] deserialization_result deserialize(
+        const std::byte* buffer, const std::size_t buffer_size);
+
+    [[nodiscard]] serialization_result serialize(
+        std::byte* buffer, const std::byte* const buffer_end);
+
+    [[nodiscard]] deserialization_result deserialize(
+        const std::byte* buffer, const std::byte* const buffer_end);
 };
 
 } // namespace hg
