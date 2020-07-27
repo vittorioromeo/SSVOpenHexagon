646d9d5348c8d9dbad41ff17a5e770ceb004a258
Vittorio Romeo <vittorio.romeo@outlook.com>
2020-07-27 16:34:57 +0100

    Rebalance Slither

--- _RELEASE/Packs/hypercube/Scripts/Levels/slither.lua
+++ _RELEASE/Packs/hypercube/Scripts/Levels/slither.lua
@@ -5,12 +5,6 @@ u_execScript("commonpatterns.lua")
 u_execScript("nextpatterns.lua")
 u_execScript("evolutionpatterns.lua")
 
--- this function adds a pattern to the timeline based on a key
-function addPattern(mKey)
-		if mKey == 0 then pAltBarrage(math.random(1, 3), 2)
-	end
-end
-
 -- shuffle the keys, and then call them to add all the patterns
 -- shuffling is better than randomizing - it guarantees all the patterns will be called
 keys = { 0 }
@@ -26,31 +20,56 @@ incrementTime = 10
 
 range = "("..(smin * 2).."/"..(smax * 2).."]"
 
+function slitherSpiralAcc()
+	t_wait(getPerfectDelayDM(THICKNESS) * 2.1)
+	t_wait(getPerfectDelayDM(THICKNESS) * 2.1)
+	local side = getRandomSide()
+
+	local acc = math.random(50, 90) / 500.0 * getRandomDir()
+	local minimum = math.random(15, 21) / 10.0 * -1
+	local maximum = -minimum
+
+	t_wait(getPerfectDelayDM(THICKNESS) * 3.1)
+
+	for i = 0, math.random(6, 10) do
+		hmcSimpleSpinnerSAcc(side, 0, acc, minimum, maximum, true)
+		t_wait(getPerfectDelay(THICKNESS) * 0.55)
+	end
+
+	t_wait(getPerfectDelayDM(THICKNESS) * 5.3)
+end
+
 -- onInit is an hardcoded function that is called when the level is first loaded
 function onInit()
 	l_setSpeedMult(1.7)
 	l_setSpeedInc(0.1)
+	l_setSpeedMax(2.9)
+
 	l_setRotationSpeed(0.2)
 	l_setRotationSpeedMax(0.4)
 	l_setRotationSpeedInc(0.035)
+
 	l_setDelayMult(1.1)
 	l_setDelayInc(0.0)
+
 	l_setFastSpin(0.0)
+
 	l_setSides(3)
 	l_setSidesMin(3)
 	l_setSidesMax(3)
+
 	l_setIncTime(10)
 
 	l_setWallAngleLeft(-25)
 
 	l_setPulseMin(60)
 	l_setPulseMax(80)
-	l_setPulseSpeed(3.6)
-	l_setPulseSpeedR(1.45)
-	l_setPulseDelayMax(7)
+	l_setPulseSpeed(3.3)
+	l_setPulseSpeedR(1.30)
+	l_setPulseDelayMax(38)
 
 	l_setBeatPulseMax(15)
-	l_setBeatPulseDelayMax(21.8)
+	l_setBeatPulseDelayMax(23.4)
 
 	l_setSwapEnabled(true)
 	l_addTracked("level", "level")
@@ -67,7 +86,7 @@ end
 -- onStep should contain your pattern spawning logic
 function onStep()
 	l_setSides(math.random(smin, smax) * 2)
-	hmcDefSpinnerSpiralAcc()
+	slitherSpiralAcc()
 end
 
 
