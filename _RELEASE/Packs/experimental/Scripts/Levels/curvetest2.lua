2454cc000a1ead19e104a0876ef1a4c22878a49e
Vittorio Romeo <vittorio.romeo@outlook.com>
2020-07-27 18:15:45 +0100

    Curve test levels

---
+++ _RELEASE/Packs/experimental/Scripts/Levels/curvetest2.lua
@@ -0,0 +1,60 @@
+-- include useful files
+u_execScript("utils.lua")
+u_execScript("common.lua")
+u_execScript("commonpatterns.lua")
+u_execScript("nextpatterns.lua")
+u_execScript("evolutionpatterns.lua")
+
+-- onInit is an hardcoded function that is called when the level is first loaded
+function onInit()
+	l_setSpeedMult(5.0)
+	l_setSpeedInc(0.0)
+	l_setRotationSpeed(0.0)
+	l_setRotationSpeedMax(0.0)
+	l_setRotationSpeedInc(0.0)
+	l_setDelayMult(1.35)
+	l_setDelayInc(0.0)
+	l_setFastSpin(71.0)
+	l_setSides(32)
+	l_setSidesMin(32)
+	l_setSidesMax(32)
+	l_setIncTime(10)
+
+	l_setWallSkewLeft(15)
+
+	l_setPulseMin(75)
+	l_setPulseMax(75)
+	l_setPulseSpeed(0)
+	l_setPulseSpeedR(0)
+	l_setPulseDelayMax(6.8)
+
+	l_setBeatPulseMax(20)
+	l_setBeatPulseDelayMax(26.1)
+
+	l_setSwapEnabled(true)
+end
+
+-- onLoad is an hardcoded function that is called when the level is started/restarted
+function onLoad()
+	m_messageAdd("remember, swap with spacebar!", 120)
+end
+
+-- onStep is an hardcoded function that is called when the level timeline is empty
+-- onStep should contain your pattern spawning logic
+function onStep()
+    hmcSimpleBarrageSNeigh(getRandomSide(), 3, 0)
+    t_wait(getPerfectDelayDM(THICKNESS) * 6)
+end
+
+
+-- onIncrement is an hardcoded function that is called when the level difficulty is incremented
+function onIncrement()
+end
+
+-- onUnload is an hardcoded function that is called when the level is closed/restarted
+function onUnload()
+end
+
+-- onUpdate is an hardcoded function that is called every frame
+function onUpdate(mFrameTime)
+end
