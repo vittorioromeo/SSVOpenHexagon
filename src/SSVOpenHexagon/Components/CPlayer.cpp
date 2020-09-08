e738e3bc90ded7a589fb0d2dd685606186ec2b4c
IvoryDuke <55557854+IvoryDuke@users.noreply.github.com>
2020-09-09 00:39:51 +0200

    Update CPlayer.cpp

--- src/SSVOpenHexagon/Components/CPlayer.cpp
+++ src/SSVOpenHexagon/Components/CPlayer.cpp
@@ -10,7 +10,7 @@ namespace hg
 
 inline constexpr float baseThickness{5.f};
 
-CPlayer::CPlayer(const sf::Vector2f& mPos, const float swapCooldown) noexcept
+CPlayer::CPlayer(const sf::Vector2f& mStartPos, const float swapCooldown) noexcept
     : startPos{mPos}, pos{mPos}, lastPos{mPos}, hue{0}, angle{0}, lastAngle{0},
       size{Config::getPlayerSize()}, speed{Config::getPlayerSpeed()},
       focusSpeed{Config::getPlayerFocusSpeed()}, dead{false},
