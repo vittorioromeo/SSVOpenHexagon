8beb1b7b7de3e414d6753e5acc5c5ceff22c22ea
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-02-28 22:10:21 +0000

    Detect death in tutorial, add onPreDeath, make player death effect deterministic

--- _RELEASE/Packs/cube/Scripts/Levels/babysteps.lua
+++ _RELEASE/Packs/cube/Scripts/Levels/babysteps.lua
@@ -18,15 +18,16 @@ keys = { 0, 1, 2, 3 }
 shuffle(keys)
 index = 0
 achievementUnlocked = false
+challengeFailed = false
 
 -- onInit is an hardcoded function that is called when the level is first loaded
 function onInit()
-	l_setSpeedMult(1.30)
+	l_setSpeedMult(1.00)
 	l_setSpeedInc(0.30)
 	l_setRotationSpeed(0.04)
 	l_setRotationSpeedMax(0.4)
 	l_setRotationSpeedInc(0.04)
-	l_setDelayMult(1.0)
+	l_setDelayMult(1.3)
 	l_setDelayInc(0.0)
 	l_setFastSpin(0.0)
 	l_setSides(6)
@@ -38,25 +39,10 @@ end
 
 -- onLoad is an hardcoded function that is called when the level is started/restarted
 function onLoad()
-	e_messageAddImportant("welcome to open hexagon 2", 130)
-	e_messageAddImportant("use left/right to rotate", 130)
-	e_messageAddImportant("avoid the walls!", 130)
-	e_stopTimeS(6)
-	e_waitS(6)
-
-	e_waitUntilS(10)
-	e_stopTimeS(5)
-	e_messageAddImportant("great job!", 130)
-	e_messageAddImportant("after a while, things get harder", 130)
-	e_messageAddImportant("get to 45 seconds to win!", 130)
-
-	e_waitUntilS(42)
-	e_eval([[l_setIncEnabled(false);]])
-	e_messageAddImportant("well done!", 130)
-	e_messageAddImportant("now play some real levels!", 138)
-
-	e_waitUntilS(45)
-	e_kill()
+	e_messageAddImportant("welcome to open hexagon 2", 120)
+	e_messageAddImportant("use left/right to rotate", 120)
+	e_messageAddImportant("avoid the walls!", 110)
+	e_stopTimeS(4)
 end
 
 -- onStep is an hardcoded function that is called when the level timeline is empty
@@ -79,10 +65,57 @@ end
 function onUnload()
 end
 
+step0 = false
+step1 = false
+step2 = false
+
 -- onUpdate is an hardcoded function that is called every frame
 function onUpdate(mFrameTime)
 	if not achievementUnlocked and l_getLevelTime() > 40 then
 		steam_unlockAchievement("a0_babysteps")
 		achievementUnlocked = true
+	end
+
+	if not step0 and l_getLevelTime() > 10 then
+		step0 = true
+
+		e_stopTimeS(5)
+
+		if not challengeFailed then
+			e_messageAddImportant("great job!", 110)
+		else
+			e_messageAddImportant("nice try...", 110)
+		end
+
+		e_messageAddImportant("after a while, things get harder", 130)
+		e_messageAddImportant("get to 45 seconds to win!", 120)
+	end
+
+	if not step1 and l_getLevelTime() > 42 then
+		step1 = true
+
+		l_setIncEnabled(false);
+
+		if challengeFailed == false then
+			e_messageAddImportant("well done! you survived!", 130)
+			e_messageAddImportant("now play some real levels!", 138)
+		else
+			e_messageAddImportant("nice try, but not quite...", 130)
+			e_messageAddImportant("try again!", 120)
+		end
+	end
+
+	if not step2 and l_getLevelTime() > 45 then
+		step2 = true
+		e_kill()
+	end
+end
+
+-- onPreDeath is an hardcoded function that is called when the player is killed, even
+-- in tutorial mode
+function onPreDeath()
+	if challengeFailed == false and l_getLevelTime() < 42 then
+		challengeFailed = true
+		e_messageAddImportant("whoops!", 60)
 	end
 end
