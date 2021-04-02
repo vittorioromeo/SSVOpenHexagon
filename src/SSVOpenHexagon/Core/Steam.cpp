c56b6f8bdeceea3239dcbabd75d50d0daf16b856
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-04-02 19:02:03 +0100

    Get Steam ID from encrypted app ticket

--- src/SSVOpenHexagon/Core/Steam.cpp
+++ src/SSVOpenHexagon/Core/Steam.cpp
@@ -4,15 +4,36 @@
 
 #include "SSVOpenHexagon/Core/Steam.hpp"
 
+#include "SSVOpenHexagon/Global/Assert.hpp"
+
 #include <SSVUtils/Core/Log/Log.hpp>
 
 #include "steam/steam_api.h"
+#include "steam/steam_api_flat.h"
+#include "steam/steamencryptedappticket.h"
 
 #include <array>
 #include <charconv>
 
 namespace hg::Steam
 {
+
+[[nodiscard]] static std::optional<CSteamID> get_user_steam_id()
+{
+    // Using C API here because C++ one doesn't work with MinGW.
+
+    ISteamUser* steam_user = SteamAPI_SteamUser_v021();
+
+    if(!SteamAPI_ISteamUser_BLoggedOn(steam_user))
+    {
+        ssvu::lo("Steam")
+            << "Attempted to retrieve Steam ID when not logged in\n";
+
+        return std::nullopt;
+    }
+
+    return CSteamID{SteamAPI_ISteamUser_GetSteamID(steam_user)};
+}
 
 [[nodiscard]] static bool initialize_steamworks()
 {
@@ -21,6 +42,18 @@ namespace hg::Steam
     if(SteamAPI_Init())
     {
         ssvu::lo("Steam") << "Steam API successfully initialized\n";
+
+        if(const std::optional<CSteamID> user_steam_id = get_user_steam_id();
+            user_steam_id.has_value())
+        {
+            ssvu::lo("Steam") << "User Steam ID: '"
+                              << user_steam_id->ConvertToUint64() << "'\n";
+        }
+        else
+        {
+            ssvu::lo("Steam") << "Could not retrieve user Steam ID\n";
+        }
+
         return true;
     }
 
@@ -91,7 +124,8 @@ void steam_manager::load_workshop_data()
 }
 
 steam_manager::steam_manager()
-    : _initialized{initialize_steamworks()}, _got_stats{false}
+    : _initialized{initialize_steamworks()}, _got_stats{false},
+      _got_ticket_response{false}, _got_ticket{false}, _ticket_steam_id{}
 {
     if(!_initialized)
     {
@@ -107,6 +141,11 @@ steam_manager::~steam_manager()
     {
         shutdown_steamworks();
     }
+}
+
+[[nodiscard]] bool steam_manager::is_initialized() const noexcept
+{
+    return _initialized;
 }
 
 bool steam_manager::request_stats_and_achievements()
@@ -166,6 +205,8 @@ bool steam_manager::unlock_achievement(std::string_view name)
 {
     if(!_initialized)
     {
+        ssvu::lo("Steam")
+            << "Attempted to unlock achievement when uninitialized\n";
         return false;
     }
 
@@ -428,6 +469,181 @@ void steam_manager::for_workshop_pack_folders(
     {
         f(s);
     }
+}
+
+static std::uint32_t unSecretData = 123456;
+
+bool steam_manager::request_encrypted_app_ticket()
+{
+    if(!_initialized)
+    {
+        ssvu::lo("Steam")
+            << "Attempted to request encrypted app ticket when uninitialized\n";
+
+        return false;
+    }
+#if __has_include("SSVOpenHexagon/Online/SecretSteamKey.hpp")
+    const SteamAPICall_t handle = SteamUser()->RequestEncryptedAppTicket(
+        &unSecretData, sizeof(unSecretData));
+
+    _encrypted_app_ticket_response_call_result.Set(
+        handle, this, &steam_manager::on_encrypted_app_ticket_response);
+
+    return true;
+#else
+    ssvu::lo("Steam")
+        << "Attempted to request encrypted app ticket without secret key\n";
+
+    return false;
+#endif
+}
+
+void steam_manager::on_encrypted_app_ticket_response(
+    EncryptedAppTicketResponse_t* data, bool io_failure)
+{
+#if __has_include("SSVOpenHexagon/Online/SecretSteamKey.hpp")
+    ssvu::lo("Steam") << "Received encrypted app ticket response\n";
+    _got_ticket_response = true;
+
+    if(io_failure)
+    {
+        ssvu::lo("Steam")
+            << "Error: encrypted app ticket response IO failure\n";
+
+        return;
+    }
+
+    if(data->m_eResult == k_EResultNoConnection)
+    {
+        ssvu::lo("Steam")
+            << "Error: requested encrypted app ticket while not connected to "
+               "Steam\n";
+
+        return;
+    }
+
+    if(data->m_eResult == k_EResultDuplicateRequest)
+    {
+        ssvu::lo("Steam")
+            << "Error: requested encrypted app ticket while there is already a "
+               "pending request\n";
+
+        return;
+    }
+
+    if(data->m_eResult == k_EResultLimitExceeded)
+    {
+        ssvu::lo("Steam") << "Error: requested encrypted app ticket more than "
+                             "once per minute\n";
+
+        return;
+    }
+
+    if(data->m_eResult != k_EResultOK)
+    {
+        ssvu::lo("Steam")
+            << "Error: requested encrypted app ticket, got unexpected result '"
+            << data->m_eResult << "'\n";
+
+        return;
+    }
+
+    SSVOH_ASSERT(data->m_eResult == k_EResultOK);
+
+    std::uint8_t rgubTicket[1024];
+    std::uint32_t cubTicket;
+
+    if(!SteamUser()->GetEncryptedAppTicket(
+           rgubTicket, sizeof(rgubTicket), &cubTicket))
+    {
+        ssvu::lo("Steam") << "Error: 'GetEncryptedAppTicket' failed\n";
+        return;
+    }
+
+    std::uint8_t rgubDecrypted[1024];
+    std::uint32_t cubDecrypted = sizeof(rgubDecrypted);
+
+    // clang-format off
+    constexpr std::uint8_t rgubKey[k_nSteamEncryptedAppTicketSymmetricKeyLen] =
+    {
+        #include "SSVOpenHexagon/Online/SecretSteamKey.hpp"
+    };
+    // clang-format on
+
+    if(!SteamEncryptedAppTicket_BDecryptTicket(rgubTicket, cubTicket,
+           rgubDecrypted, &cubDecrypted, rgubKey, sizeof(rgubKey)))
+    {
+        ssvu::lo("Steam") << "Error: 'BDecryptTicket' failed\n";
+        return;
+    }
+
+    if(!SteamEncryptedAppTicket_BIsTicketForApp(
+           rgubDecrypted, cubDecrypted, SteamUtils()->GetAppID()))
+    {
+        ssvu::lo("Steam") << "Error: ticket for wrong app id\n";
+        return;
+    }
+
+    CSteamID steamIDFromTicket;
+    SteamEncryptedAppTicket_GetTicketSteamID(
+        rgubDecrypted, cubDecrypted, &steamIDFromTicket);
+
+    if(const std::optional<CSteamID> user_steam_id = get_user_steam_id();
+        user_steam_id.has_value())
+    {
+        if(steamIDFromTicket != *user_steam_id)
+        {
+            ssvu::lo("Steam") << "Error: ticket for wrong user\n";
+            return;
+        }
+        else
+        {
+            ssvu::lo("Steam") << "Steam ID ticket matches user Steam ID\n";
+        }
+    }
+    else
+    {
+        ssvu::lo("Steam") << "Could not retrieve user Steam ID\n";
+        return;
+    }
+
+    std::uint32_t cubData;
+    std::uint32_t* pUnSecretData =
+        (std::uint32_t*)SteamEncryptedAppTicket_GetUserVariableData(
+            rgubDecrypted, cubDecrypted, &cubData);
+
+    if(cubData != sizeof(std::uint32_t) || *pUnSecretData != unSecretData)
+    {
+        ssvu::lo("Steam") << "Error: failed to retrieve secret data\n";
+    }
+
+    _got_ticket = true;
+    _ticket_steam_id = steamIDFromTicket;
+
+    ssvu::lo("Steam") << "GetEncryptedAppTicket succeeded (steamId: '"
+                      << steamIDFromTicket.ConvertToUint64() << "')\n";
+#else
+    _got_ticket_response = true;
+    _got_ticket = false;
+    _ticket_steam_id = std::nullopt;
+#endif
+}
+
+[[nodiscard]] bool
+steam_manager::got_encrypted_app_ticket_response() const noexcept
+{
+    return _got_ticket_response;
+}
+
+[[nodiscard]] bool steam_manager::got_encrypted_app_ticket() const noexcept
+{
+    return _got_ticket;
+}
+
+[[nodiscard]] std::optional<CSteamID>
+steam_manager::get_ticket_steam_id() const noexcept
+{
+    return _ticket_steam_id;
 }
 
 } // namespace hg::Steam
