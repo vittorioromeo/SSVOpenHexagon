c718760ac0db7d69cbdaf2b4cd273ddfe7bba206
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-04-16 21:28:20 +0100

    more conservative optimization flags

--- buildlx/make_release_server_vbox_0_cmake.sh
+++ buildlx/make_release_server_vbox_0_cmake.sh
@@ -14,7 +14,5 @@ cmake .. -G"Ninja" \
          -DCMAKE_CXX_FLAGS="\
              -Wall -Wextra -Wpedantic -Wno-braced-scalar-init \
              -Wno-pragmas -Wno-missing-field-initializers \
-             -O3 -DNDEBUG -fallow-store-data-races \
-             -fno-math-errno -ffinite-math-only -fno-rounding-math \
-             -fno-signaling-nans -fcx-limited-range -fexcess-precision=fast \
-             -fno-unsafe-math-optimizations"
+             -O3 -DNDEBUG"
+
--- buildrel/make_release_client_win10_msys_0_cmake.sh
+++ buildrel/make_release_client_win10_msys_0_cmake.sh
@@ -14,7 +14,4 @@ cmake .. -G"MinGW Makefiles" \
          -DCMAKE_CXX_FLAGS="\
              -Wall -Wextra -Wpedantic -Wno-braced-scalar-init \
              -Wno-pragmas -Wno-missing-field-initializers \
-             -O3 -DNDEBUG -fallow-store-data-races \
-             -fno-math-errno -ffinite-math-only -fno-rounding-math \
-             -fno-signaling-nans -fcx-limited-range -fexcess-precision=fast \
-             -fno-unsafe-math-optimizations"
+             -O3 -DNDEBUG"
