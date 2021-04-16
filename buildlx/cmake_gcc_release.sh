74b58b9eddeb299b1b26fb0e05aac8c7166867cc
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-04-16 16:04:30 +0100

    build stuff

--- buildlx/cmake_gcc_release.sh
+++ buildlx/cmake_gcc_release.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-cmake .. -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations -Wall -Wextra -Wpedantic"
-
-
+cmake .. -G"Ninja" \
+         -DCMAKE_BUILD_TYPE=RELEASE \
+         -DCMAKE_C_COMPILER="gcc" \
+         -DCMAKE_CXX_COMPILER="g++" \
+         -DCMAKE_CXX_FLAGS="\
+             -Wall -Wextra -Wpedantic -Wno-braced-scalar-init \
+             -Wno-pragmas -Wno-missing-field-initializers \
+             -O3 -DNDEBUG -fallow-store-data-races \
+             -fno-math-errno -ffinite-math-only -fno-rounding-math \
+             -fno-signaling-nans -fcx-limited-range -fexcess-precision=fast \
+             -fno-unsafe-math-optimizations"
