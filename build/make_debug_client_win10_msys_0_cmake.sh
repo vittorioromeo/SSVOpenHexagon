4e1b68afe6427c1aee57a71da7f62b9964fc71e3
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-04-18 21:42:19 +0100

    Scripts

---
+++ build/make_debug_client_win10_msys_0_cmake.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+echo ""
+echo ""
+echo "--------------------------------------------------------------------"
+echo "| RUNNING CMAKE IN DEBUG MODE (MINGW MAKEFILES)                  |"
+echo "--------------------------------------------------------------------"
+cmake .. -G"MinGW Makefiles" \
+         -DCMAKE_BUILD_TYPE=DEBUG \
+         -DCMAKE_C_COMPILER="gcc" \
+         -DCMAKE_CXX_COMPILER="g++" \
+         -DCMAKE_CXX_FLAGS="-O0 -fno-omit-frame-pointer \
+            -Wall -Wextra -Wpedantic -Wno-braced-scalar-init \
+            -D_GLIBCXX_ASSERTIONS=1  -D_FORTIFY_SOURCE=2 \
+            -fstack-protector -Wno-pragmas"
+
