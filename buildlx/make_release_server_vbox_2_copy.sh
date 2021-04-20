569c28a8f90b7c60c687e911e9207a0c60a854d0
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-04-20 16:08:41 +0100

    scripts

--- buildlx/make_release_server_vbox_2_copy.sh
+++ buildlx/make_release_server_vbox_2_copy.sh
@@ -8,3 +8,6 @@ echo "--------------------------------------------------------------------"
 echo "| COPYING TO WINDOWS DRIVE AS 'SSVOpenHexagonVbox'                 |"
 echo "--------------------------------------------------------------------"
 cp ./SSVOpenHexagon /media/sf_C_DRIVE/OHWorkspace/SSVOpenHexagon/_RELEASE/SSVOpenHexagonVbox
+cp ./SSVOpenHexagon ../_RELEASE/SSVOpenHexagon
+cp ./SSVOpenHexagon ../_RELEASE/SSVOpenHexagonVbox
+
