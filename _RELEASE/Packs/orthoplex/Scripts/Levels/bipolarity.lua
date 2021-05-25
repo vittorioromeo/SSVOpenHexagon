43d772ac903565132f39f09f43ebd95a92626215
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-05-25 03:10:31 +0100

    Add achievements for Bipolarity

--- _RELEASE/Packs/orthoplex/Scripts/Levels/bipolarity.lua
+++ _RELEASE/Packs/orthoplex/Scripts/Levels/bipolarity.lua
@@ -13,6 +13,8 @@ lastRotationDir = 0
 swapped = false
 rotSpeed = 0.25
 rotSpeedMax = 0.9
+achievementUnlocked = false
+hardAchievementUnlocked = false
 
 FloatingWall = {}
 FloatingWall.__index = FloatingWall
@@ -296,6 +298,16 @@ function onUpdate(mFrameTime)
                 l_setSpeedMult(l_getSpeedMax())
             end
         end
+    end
+
+    if not achievementUnlocked and l_getLevelTime() > 45 and u_getDifficultyMult() >= 1 then
+        steam_unlockAchievement("a36_bipolarity")
+        achievementUnlocked = true
+    end
+
+    if not hardAchievementUnlocked and l_getLevelTime() > 30 and u_getDifficultyMult() > 1.5 then
+        steam_unlockAchievement("a37_bipolarity_hard")
+        hardAchievementUnlocked = true
     end
 end
 
