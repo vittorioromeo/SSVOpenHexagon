96621f5c7ccc7292af28bdf8afc20bae78c23c34
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-10-21 18:22:00 +0100

    Fixup some graphical issues with text, bump version to 2.0.7

--- _RELEASE/Packs/base/Scripts/nextpatterns.lua
+++ _RELEASE/Packs/base/Scripts/nextpatterns.lua
@@ -5,33 +5,33 @@ u_execScript("alternativepatterns.lua")
 
 -- getPerfectAccelDM: Returns a constant that is used to perfectly adjust wallAcc acceleration to speed and difficulty multiplier
 function getPerfectAccelDM()
-	local requiredDecel = (u_getSpeedMultDM() ^ 2.02);
-	local diffAdjust = (u_getDifficultyMult() ^ 0.65);
-	if (u_getDifficultyMult() < 1) then
-		if (u_getDifficultyMult() < 0.5) then
-			diffAdjust = 1.2 * (0.256289 * math.sin(u_getDifficultyMult()) - 36.5669 * math.cos(u_getDifficultyMult()) + 36.5674);
-		else
-			diffAdjust = 1 - (-2 * u_getDifficultyMult() + 2) ^ 2 / 2.7;
-		end
-	end
-	return diffAdjust * requiredDecel
+    local requiredDecel = (u_getSpeedMultDM() ^ 2.02);
+    local diffAdjust = (u_getDifficultyMult() ^ 0.65);
+    if (u_getDifficultyMult() < 1) then
+        if (u_getDifficultyMult() < 0.5) then
+            diffAdjust = 1.2 * (0.256289 * math.sin(u_getDifficultyMult()) - 36.5669 * math.cos(u_getDifficultyMult()) + 36.5674);
+        else
+            diffAdjust = 1 - (-2 * u_getDifficultyMult() + 2) ^ 2 / 2.7;
+        end
+    end
+    return diffAdjust * requiredDecel
 end
 
 -- wallSAdj: Returns a wall with common THICKNESS and multiplies the speed by mAdj
 function wallSAdj(mSide, mAdj) w_wallAdj(mSide, THICKNESS, mAdj) end
 
 -- wallSAcc: Returns a wallAcc perfectly adjusted to speed and difficulty multiplier
-function wallSAcc(mSide, mAdj, mAcc, mMinSpd, mMaxSpd) 
-	local speedMult = u_getSpeedMultDM()
-	local adjust = getPerfectAccelDM()
-	w_wallAcc(mSide, THICKNESS, mAdj * speedMult, mAcc * adjust, mMinSpd, mMaxSpd) 
+function wallSAcc(mSide, mAdj, mAcc, mMinSpd, mMaxSpd)
+    local speedMult = u_getSpeedMultDM()
+    local adjust = getPerfectAccelDM()
+    w_wallAcc(mSide, THICKNESS, mAdj * speedMult, mAcc * adjust, mMinSpd, mMaxSpd)
 end
 
 -- wallSHAcc: Creates a wallHModSpeedData adjusted the perfect acceleration adjustment
-function wallSHAcc(mHMod, mSide, mAdj, mAcc, mMinSpd, mMaxSpd, mPingPong) 
-	local speedMult = u_getSpeedMultDM()
-	local adjust = getPerfectAccelDM()
-	w_wallHModSpeedData(mHMod, mSide, THICKNESS, mAdj * speedMult, mAcc * adjust, mMinSpd, mMaxSpd, mPingPong) 
+function wallSHAcc(mHMod, mSide, mAdj, mAcc, mMinSpd, mMaxSpd, mPingPong)
+    local speedMult = u_getSpeedMultDM()
+    local adjust = getPerfectAccelDM()
+    w_wallHModSpeedData(mHMod, mSide, THICKNESS, mAdj * speedMult, mAcc * adjust, mMinSpd, mMaxSpd, mPingPong)
 end
 
 -----------------------------------------
@@ -43,67 +43,67 @@ end
 -- A trap version of a barrage
 -- mSide: The side the pattern will spawn on
 function pTrapBarrage(mSide)
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-		
-	cBarrage(mSide)
-	t_wait(delay * 3)
-	wallSAdj(mSide, 1.9)
+    local delay = getPerfectDelay(THICKNESS) * 3.7
 
-	t_wait(delay * 2.5)
+    cBarrage(mSide)
+    t_wait(delay * 3)
+    wallSAdj(mSide, 1.9)
+
+    t_wait(delay * 2.5)
 end
 
 -- A trap barrage with two gaps, both on opposing sides of each other
 -- mSide: The side the pattern will spawn on
 function pTrapBarrageDouble(mSide)
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-	local side2 = mSide + getHalfSides();
-	
-	for i = 0, l_getSides() - 1 do
-		local currentSide = mSide + i
-		if((currentSide ~= mSide) and (currentSide ~= side2)) then cWall(currentSide) end
-	end
+    local delay = getPerfectDelay(THICKNESS) * 3.7
+    local side2 = mSide + getHalfSides();
 
-	t_wait(delay * 3)
-	wallSAdj(mSide, 1.9)
-	wallSAdj(side2, 1.9)
-	
-	t_wait(delay * 2.5)
+    for i = 0, l_getSides() - 1 do
+        local currentSide = mSide + i
+        if((currentSide ~= mSide) and (currentSide ~= side2)) then cWall(currentSide) end
+    end
+
+    t_wait(delay * 3)
+    wallSAdj(mSide, 1.9)
+    wallSAdj(side2, 1.9)
+
+    t_wait(delay * 2.5)
 end
 
 -- pTrapBarrage, but the whole barrage is speeding instead of the "trap".
 -- mSide: The side the pattern will spawn on
 function pTrapBarrageInverse(mSide)
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-	
-	cWall(mSide)	
-	t_wait(delay * 3)
+    local delay = getPerfectDelay(THICKNESS) * 3.7
 
-	for i = 0, l_getSides() - 1 do
-		local currentSide = mSide + i
-		if(currentSide ~= mSide) then wallSAdj(currentSide, 1.9) end
-	end
+    cWall(mSide)
+    t_wait(delay * 3)
 
-	t_wait(delay * 2.5)
+    for i = 0, l_getSides() - 1 do
+        local currentSide = mSide + i
+        if(currentSide ~= mSide) then wallSAdj(currentSide, 1.9) end
+    end
+
+    t_wait(delay * 2.5)
 end
 
 -- A trap version of an alt barrage
 -- mSide: The side the pattern will spawn on
 function pTrapBarrageAlt(mSide)
-	local delay = getPerfectDelay(THICKNESS) * 3.7
+    local delay = getPerfectDelay(THICKNESS) * 3.7
 
-	for i = 0, l_getSides() - 1 do
-		local currentSide = mSide + i
-		if(currentSide % 2 ~= 0) then cWall(currentSide) end
-	end
+    for i = 0, l_getSides() - 1 do
+        local currentSide = mSide + i
+        if(currentSide % 2 ~= 0) then cWall(currentSide) end
+    end
 
-	t_wait(delay * 3)
+    t_wait(delay * 3)
 
-	for i = 0, l_getSides() - 1 do
-		local currentSide = mSide + i
-		if(currentSide % 2 == 0) then wallSAdj(currentSide, 1.9) end
-	end
+    for i = 0, l_getSides() - 1 do
+        local currentSide = mSide + i
+        if(currentSide % 2 == 0) then wallSAdj(currentSide, 1.9) end
+    end
 
-	t_wait(delay * 2.5)
+    t_wait(delay * 2.5)
 end
 
 --------------------------------------------
@@ -112,47 +112,47 @@ end
 
 -- Spawns a barrage with two opposing holes instead of one
 function pRCBarrageDouble()
-	local currentSides = l_getSides()
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-	local startSide = getRandomSide()
+    local currentSides = l_getSides()
+    local delay = getPerfectDelay(THICKNESS) * 3.7
+    local startSide = getRandomSide()
 
-	for i = 0, currentSides - 2 do
-		local currentSide = startSide + i
-		local holeSide = startSide + i + (currentSides / 2)
-		if(i ~= holeSide) then cWall(currentSide) end
-	end
-	t_wait(delay * 2.5)
+    for i = 0, currentSides - 2 do
+        local currentSide = startSide + i
+        local holeSide = startSide + i + (currentSides / 2)
+        if(i ~= holeSide) then cWall(currentSide) end
+    end
+    t_wait(delay * 2.5)
 end
 
 -- Summons a series of barrages, but with the side count ascending, adding one new side for each barrage summoned.
 -- This as a result can cause the barrages to either spiral or stay still, depending on the side it spawns on
 -- This pattern can be unpredictable in it's nature.
 function pRCAscendBarrage(mSide, mStartSides, mEndSides)
-	local delay = getPerfectDelay(THICKNESS) * 5.6
-	for i = mStartSides, mEndSides do
-		t_eval([[l_setSides(]]..i..[[)]])
-		for r = 0, i - 2 do
-			cWall(r + mSide)
-		end
-		t_wait(delay)
-	end
-	t_wait(delay)
+    local delay = getPerfectDelay(THICKNESS) * 5.6
+    for i = mStartSides, mEndSides do
+        t_eval([[l_setSides(]]..i..[[)]])
+        for r = 0, i - 2 do
+            cWall(r + mSide)
+        end
+        t_wait(delay)
+    end
+    t_wait(delay)
 end
 
 -- Summons a series of barrages in randomized positions, with the side count ascending per barrage
 -- mStartSides: The starting number of sides for the pattern
 -- mEndSides: The ending number of sides for the pattern
 function pRCAscendBarrageRandom(mStartSides, mEndSides)
-	local delay = getPerfectDelay(THICKNESS) * 8.5
-	for i = mStartSides, mEndSides do
-		t_eval([[l_setSides(]]..i..[[)]])
-		local offset = math.random(1, i)
-		for r = 0, i - 2 do
-			cWall(r + offset)
-		end
-		t_wait(delay)
-	end
-	t_wait(delay)
+    local delay = getPerfectDelay(THICKNESS) * 8.5
+    for i = mStartSides, mEndSides do
+        t_eval([[l_setSides(]]..i..[[)]])
+        local offset = math.random(1, i)
+        for r = 0, i - 2 do
+            cWall(r + offset)
+        end
+        t_wait(delay)
+    end
+    t_wait(delay)
 end
 
 -- Summons a series of alt barrages, but the side count randomizes per alt barrage to make the pattern much harder to navigate
@@ -161,21 +161,21 @@ end
 -- mLowerSides: The lowest side count to select at random
 -- mUpperSides: The highest side count to select at random
 function pRCDynamicAltBarrage(mStep, mTimes, mLowerSides, mUpperSides)
-	local delay = getPerfectDelay(THICKNESS) * 5.6
-	local prevSides = 0
-	
-	for r = 0, mTimes do
-		local sides = math.random(mLowerSides, mUpperSides)
-		if (sides ~= prevSides) then
-			t_eval([[l_setSides(]]..sides..[[)]])
-		end
-		prevSides = sides
-		for i = 0, sides, mStep do
-			cWall(i + r)
-		end
-		t_wait(delay)
-	end
-	t_wait(delay)
+    local delay = getPerfectDelay(THICKNESS) * 5.6
+    local prevSides = 0
+
+    for r = 0, mTimes do
+        local sides = math.random(mLowerSides, mUpperSides)
+        if (sides ~= prevSides) then
+            t_eval([[l_setSides(]]..sides..[[)]])
+        end
+        prevSides = sides
+        for i = 0, sides, mStep do
+            cWall(i + r)
+        end
+        t_wait(delay)
+    end
+    t_wait(delay)
 end
 
 -------------------------------------------
@@ -184,189 +184,189 @@ end
 
 -- pACBarrageAccelerate: A barrage that will slowly increase in speed over time, accelerating.
 function pACBarrageAccelerate()
-	local currentSides = l_getSides()
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-	local startSide = getRandomSide()
+    local currentSides = l_getSides()
+    local delay = getPerfectDelay(THICKNESS) * 3.7
+    local startSide = getRandomSide()
 
-	for i = 0, currentSides - 2 do
-		local currentSide = startSide + i
-		wallSAcc(currentSide, 2, 0.015, 1, 10)
-	end
-	t_wait(delay * 2.5)
+    for i = 0, currentSides - 2 do
+        local currentSide = startSide + i
+        wallSAcc(currentSide, 2, 0.015, 1, 10)
+    end
+    t_wait(delay * 2.5)
 end
 
 -- pACBarrageDecelerate: A barrage that will quickly approach the player and decelerate.
 function pACBarrageDecelerate()
-	local currentSides = l_getSides()
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-	local startSide = getRandomSide()
+    local currentSides = l_getSides()
+    local delay = getPerfectDelay(THICKNESS) * 3.7
+    local startSide = getRandomSide()
 
-	for i = 0, currentSides - 2 do
-		local currentSide = startSide + i
-		wallSAcc(currentSide, 10, -0.22, 0.6, 10)
-	end
-	t_wait(delay * 3.5)
+    for i = 0, currentSides - 2 do
+        local currentSide = startSide + i
+        wallSAcc(currentSide, 10, -0.22, 0.6, 10)
+    end
+    t_wait(delay * 3.5)
 end
 
 -- pACBarrageDeception: A barrage that never interacts with the player, going in and out of the view, tricking the player.
 function pACBarrageDeception(mAdjMult, mAccMult)
-	mAdjMult = mAdjMult or 1;
-	mAccMult = mAccMult or 1;
-	
-	local currentSides = l_getSides()
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-	local startSide = getRandomSide()
+    mAdjMult = mAdjMult or 1;
+    mAccMult = mAccMult or 1;
 
-	for i = 0, currentSides - 2 do
-		local currentSide = startSide + i
-		wallSAcc(currentSide, 2.8 * mAdjMult, -0.05 * mAccMult, -2, 5)
-	end
-	t_wait(delay * 4)
+    local currentSides = l_getSides()
+    local delay = getPerfectDelay(THICKNESS) * 3.7
+    local startSide = getRandomSide()
+
+    for i = 0, currentSides - 2 do
+        local currentSide = startSide + i
+        wallSAcc(currentSide, 2.8 * mAdjMult, -0.05 * mAccMult, -2, 5)
+    end
+    t_wait(delay * 4)
 end
 
 -- pACInverseBarrage: Two decelerating barrages, one slower than the other to create an inverse barrage
 function pACInverseBarrage()
-	local currentSides = l_getSides()
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-	local startSide = getRandomSide()
-	
-	for i = 0, currentSides - 2 do
-		local currentSide = startSide + i
-		wallSAcc(currentSide, 10, -0.21, 0.6, 10)
-	end
-	for i = 0, currentSides - 2 do
-		local currentSide = startSide + getHalfSides() + i
-		wallSAcc(currentSide, 10, -0.28, 0.6, 10)
-	end
-	t_wait(delay * 6.5)
+    local currentSides = l_getSides()
+    local delay = getPerfectDelay(THICKNESS) * 3.7
+    local startSide = getRandomSide()
+
+    for i = 0, currentSides - 2 do
+        local currentSide = startSide + i
+        wallSAcc(currentSide, 10, -0.21, 0.6, 10)
+    end
+    for i = 0, currentSides - 2 do
+        local currentSide = startSide + getHalfSides() + i
+        wallSAcc(currentSide, 10, -0.28, 0.6, 10)
+    end
+    t_wait(delay * 6.5)
 end
 
 -- pACBarrageMulti: A decelerating barrage, followed by a trail of accelerating barrages for visual effect
 function pACBarrageMulti()
-	local currentSides = l_getSides()
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-	local startSide = math.random(0, 10)
+    local currentSides = l_getSides()
+    local delay = getPerfectDelay(THICKNESS) * 3.7
+    local startSide = math.random(0, 10)
 
-	for i = 0, currentSides - 2 do
-		local currentSide = startSide + i
-		wallSAcc(currentSide, 10, -0.21, 0.31, 10)
-		wallSAcc(currentSide, 0, 0.01, 0, 2.0)
-		wallSAcc(currentSide, 0, 0.02, 0, 2.0)
-		wallSAcc(currentSide, 0, 0.025, 0, 2.0)
-	end
-	t_wait(delay * 8)
+    for i = 0, currentSides - 2 do
+        local currentSide = startSide + i
+        wallSAcc(currentSide, 10, -0.21, 0.31, 10)
+        wallSAcc(currentSide, 0, 0.01, 0, 2.0)
+        wallSAcc(currentSide, 0, 0.02, 0, 2.0)
+        wallSAcc(currentSide, 0, 0.025, 0, 2.0)
+    end
+    t_wait(delay * 8)
 end
 
 -- pACSpiral: A pSpiral that decelerates, followed by an accelerating barrage similar to pACBarrageMulti
 function pACSpiral()
-	local currentSides = l_getSides()
-	local oldThickness = THICKNESS;
-	THICKNESS = getPerfectThickness(THICKNESS);
-	local delay = getPerfectDelay(THICKNESS) * 3
-	local startSide = math.random(0, 10)
-	local loopDir = getRandomDir()
+    local currentSides = l_getSides()
+    local oldThickness = THICKNESS;
+    THICKNESS = getPerfectThickness(THICKNESS);
+    local delay = getPerfectDelay(THICKNESS) * 3
+    local startSide = math.random(0, 10)
+    local loopDir = getRandomDir()
 
-	for i = 0, currentSides + getHalfSides() do
-		local currentSide = startSide + i * loopDir
-		wallSAcc(currentSide, 10, -0.2, 0.40, 10)
-		t_wait((delay / 2.1))
-		wallSAcc(currentSide + (getHalfSides() * loopDir), 0, 0.128, 0, 1.4)
-	end
-	THICKNESS = oldThickness;
-	t_wait(delay * 3)
+    for i = 0, currentSides + getHalfSides() do
+        local currentSide = startSide + i * loopDir
+        wallSAcc(currentSide, 10, -0.2, 0.40, 10)
+        t_wait((delay / 2.1))
+        wallSAcc(currentSide + (getHalfSides() * loopDir), 0, 0.128, 0, 1.4)
+    end
+    THICKNESS = oldThickness;
+    t_wait(delay * 3)
 end
 
 -- A series of alt barrages that decelerate.
 -- mTimes: The number of alt barrage series to create
 function pACAltBarrage(mTimes)
-	local currentSides = l_getSides()
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-	
-	for t = 1, mTimes do
-		for i = 1, currentSides, 2 do
-			wallSAcc(i, 10, -0.21, 0.6, 10)
-		end
-		t_wait(delay * 1.6);
-		for i = 0, currentSides - 1, 2 do
-			wallSAcc(i, 10, -0.21, 0.6, 10)
-		end
-		t_wait(delay * 1.6);
-	end
-	
-	t_wait(delay * 3.5)
+    local currentSides = l_getSides()
+    local delay = getPerfectDelay(THICKNESS) * 3.7
+
+    for t = 1, mTimes do
+        for i = 1, currentSides, 2 do
+            wallSAcc(i, 10, -0.21, 0.6, 10)
+        end
+        t_wait(delay * 1.6);
+        for i = 0, currentSides - 1, 2 do
+            wallSAcc(i, 10, -0.21, 0.6, 10)
+        end
+        t_wait(delay * 1.6);
+    end
+
+    t_wait(delay * 3.5)
 end
 
 -- pACAltBarrageMulti: Alt Barrage with similar behavior to pACBarrageMulti
 function pACAltBarrageMulti()
-	local currentSides = l_getSides()
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-	local offset = math.random(0, 1);
-	
-	for i = 1, currentSides, 2 do
-		wallSAcc(i + offset, 10, -0.2, 0.5, 10)
-		wallSAcc(i + offset, 0, 0.01, 0, 4.0)
-		wallSAcc(i + offset, 0, 0.015, 0, 4.0)
-		wallSAcc(i + offset, 0, 0.02, 0, 4.0)
-	end
-	
-	t_wait(delay * 8)
+    local currentSides = l_getSides()
+    local delay = getPerfectDelay(THICKNESS) * 3.7
+    local offset = math.random(0, 1);
+
+    for i = 1, currentSides, 2 do
+        wallSAcc(i + offset, 10, -0.2, 0.5, 10)
+        wallSAcc(i + offset, 0, 0.01, 0, 4.0)
+        wallSAcc(i + offset, 0, 0.015, 0, 4.0)
+        wallSAcc(i + offset, 0, 0.02, 0, 4.0)
+    end
+
+    t_wait(delay * 8)
 end
 
 -- Reveals randomly positioned Alt Barrages for the player to navigate
 -- mTimes: The number of alt barrages to create
 function pACAltBarrageReveal(mTimes)
-	local currentSides = l_getSides()
-	local delay = getPerfectDelay(THICKNESS) * 3.7
-	local chooser = 0;
-	
-	for t = 1, mTimes do
-		chooser = math.random(0, 1);
-		for i = 1, currentSides do
-			if (i % 2 == chooser) then
-				wallSAcc(i, 10, -0.21, 0.6, 10)
-			else
-				wallSAcc(i, 10, -0.21, -2, 10)
-			end
-		end
-		t_wait(delay * 1.6);
-	end
-	
-	t_wait(delay * 3.5)
+    local currentSides = l_getSides()
+    local delay = getPerfectDelay(THICKNESS) * 3.7
+    local chooser = 0;
+
+    for t = 1, mTimes do
+        chooser = math.random(0, 1);
+        for i = 1, currentSides do
+            if (i % 2 == chooser) then
+                wallSAcc(i, 10, -0.21, 0.6, 10)
+            else
+                wallSAcc(i, 10, -0.21, -2, 10)
+            end
+        end
+        t_wait(delay * 1.6);
+    end
+
+    t_wait(delay * 3.5)
 end
 
 -- A tunnel pattern that reveals random barrages for the player to navigate
 -- mTimes: The number of tunnel segments to create
 function pACTunnelReveal(mTimes)
-	local currentSides = l_getSides()
-	local oldThickness = THICKNESS
-	local myThickness = getPerfectThickness(THICKNESS);
-	local delay = getPerfectDelay(myThickness) * 5
-	local startSide = getRandomSide()
-	local loopDir = getRandomDir()
-	
-	for t = 0, mTimes do
-		THICKNESS = myThickness + 5 * u_getSpeedMultDM() * delay
-		if t < mTimes then
-			wallSAcc(startSide, 10, -0.22, 0.6, 10)
-		end
-		
-		THICKNESS = oldThickness
-		
-		local revealSide = math.random(0, currentSides - 1)
-		if (revealSide == startSide) then
-			revealSide = (revealSide + getRandomDir()) % currentSides;
-		end
-		for i = 0, currentSides - 1 do
-			if (i == revealSide) then
-				wallSAcc(i, 10, -0.22, -2, 10)
-			else
-				wallSAcc(i, 10, -0.22, 0.6, 10)
-			end
-		end
-		t_wait(delay)
-		loopDir = loopDir * -1
-	end
-	
-	THICKNESS = oldThickness;
-	t_wait(delay)
+    local currentSides = l_getSides()
+    local oldThickness = THICKNESS
+    local myThickness = getPerfectThickness(THICKNESS);
+    local delay = getPerfectDelay(myThickness) * 5
+    local startSide = getRandomSide()
+    local loopDir = getRandomDir()
+
+    for t = 0, mTimes do
+        THICKNESS = myThickness + 5 * u_getSpeedMultDM() * delay
+        if t < mTimes then
+            wallSAcc(startSide, 10, -0.22, 0.6, 10)
+        end
+
+        THICKNESS = oldThickness
+
+        local revealSide = math.random(0, currentSides - 1)
+        if (revealSide == startSide) then
+            revealSide = (revealSide + getRandomDir()) % currentSides;
+        end
+        for i = 0, currentSides - 1 do
+            if (i == revealSide) then
+                wallSAcc(i, 10, -0.22, -2, 10)
+            else
+                wallSAcc(i, 10, -0.22, 0.6, 10)
+            end
+        end
+        t_wait(delay)
+        loopDir = loopDir * -1
+    end
+
+    THICKNESS = oldThickness;
+    t_wait(delay)
 end
--- _RELEASE/Packs/hypercube/Scripts/Levels/disc-o.lua
+++ _RELEASE/Packs/hypercube/Scripts/Levels/disc-o.lua
@@ -7,34 +7,34 @@ u_execDependencyScript("ohvrvanilla", "base", "vittorio romeo", "evolutionpatter
 
 -- this function adds a pattern to the timeline based on a key
 function addPattern(mKey)
-	-- Normal Palette
-		if mKey == 0 then pAltBarrage(math.random(2, 4), 2)
-	elseif mKey == 1 then pMirrorSpiral(math.random(2, 4), 0)
-	elseif mKey == 2 then pBarrageSpiral(math.random(0, 3), 1, 1)
-	elseif mKey == 3 then pBarrageSpiral(math.random(0, 2), 1.2, 2)
-	elseif mKey == 4 then pBarrageSpiral(2, 0.7, 1)
-	elseif mKey == 5 then pInverseBarrage(0)
-	elseif mKey == 6 then hmpBarrageSpiral(math.random(1, 3), 2, clamp(level + 1, 2, 6))
-	elseif mKey == 7 then pMirrorWallStrip(1, 0)
-	elseif mKey == 8 then hmpSpinner(1, clamp(level, 1, 6))
-	elseif mKey == 9 then hmpBarrage(1, clamp(level, 1, 6))
-	elseif mKey == 10 then hmcDef2Cage()
-	elseif mKey == 11 then hmpBarrageSpiralSpin(math.random(7, 14), 2, clamp(level + 1, 2, 6))
-	elseif mKey == 12 then hmpGrowTunnel(math.random(2, 3))
-	elseif mKey == 13 then 
-		hmcGrowBarrage(getRandomSide(), getRandomSide())
-		t_wait(getPerfectDelay(getPerfectThickness(THICKNESS)) * 4.5)
-	elseif mKey == 14 then pSwapBarrage(getRandomSide())
-	elseif mKey == 15 then pSwapCorridor(math.random(2, 3))
-	elseif mKey == 16 then hmpTwirl(math.random(2, 4), clamp(level + 1, 2, 5), 1)
-	-- Specials Palette
-	elseif mKey == 101 then 
-		hmpBarrageStop(3, 12)
-		if (u_getDifficultyMult() > 1) then
-			t_wait(getPerfectDelay(THICKNESS) * 3)
-		end
-	elseif mKey == 102 then hmpSwarm(clamp(level, 1, 6), 3 * clamp(level, 1, 6), true, level > 10, false)
-	end
+    -- Normal Palette
+        if mKey == 0 then pAltBarrage(math.random(2, 4), 2)
+    elseif mKey == 1 then pMirrorSpiral(math.random(2, 4), 0)
+    elseif mKey == 2 then pBarrageSpiral(math.random(0, 3), 1, 1)
+    elseif mKey == 3 then pBarrageSpiral(math.random(0, 2), 1.2, 2)
+    elseif mKey == 4 then pBarrageSpiral(2, 0.7, 1)
+    elseif mKey == 5 then pInverseBarrage(0)
+    elseif mKey == 6 then hmpBarrageSpiral(math.random(1, 3), 2, clamp(level + 1, 2, 6))
+    elseif mKey == 7 then pMirrorWallStrip(1, 0)
+    elseif mKey == 8 then hmpSpinner(1, clamp(level, 1, 6))
+    elseif mKey == 9 then hmpBarrage(1, clamp(level, 1, 6))
+    elseif mKey == 10 then hmcDef2Cage()
+    elseif mKey == 11 then hmpBarrageSpiralSpin(math.random(7, 14), 2, clamp(level + 1, 2, 6))
+    elseif mKey == 12 then hmpGrowTunnel(math.random(2, 3))
+    elseif mKey == 13 then
+        hmcGrowBarrage(getRandomSide(), getRandomSide())
+        t_wait(getPerfectDelay(getPerfectThickness(THICKNESS)) * 4.5)
+    elseif mKey == 14 then pSwapBarrage(getRandomSide())
+    elseif mKey == 15 then pSwapCorridor(math.random(2, 3))
+    elseif mKey == 16 then hmpTwirl(math.random(2, 4), clamp(level + 1, 2, 5), 1)
+    -- Specials Palette
+    elseif mKey == 101 then
+        hmpBarrageStop(3, 12)
+        if (u_getDifficultyMult() > 1) then
+            t_wait(getPerfectDelay(THICKNESS) * 3)
+        end
+    elseif mKey == 102 then hmpSwarm(clamp(level, 1, 6), 3 * clamp(level, 1, 6), true, level > 10, false)
+    end
 end
 
 --globalHueModifier = 0
@@ -56,33 +56,33 @@ special = "none"
 
 -- onInit is an hardcoded function that is called when the level is first loaded
 function onInit()
-	l_setSpeedMult(1.7)
-	l_setSpeedInc(0)
-	l_setSpeedMax(3.5)
-	l_setRotationSpeed(0.1)
-	l_setRotationSpeedMax(0.415)
-	l_setRotationSpeedInc(0)
-	l_setDelayMult(1.2)
-	l_setDelayInc(0.0)
-	l_setFastSpin(0.0)
-	l_setSides(6)
-	l_setSidesMin(6)
-	l_setSidesMax(6)
-	l_setIncTime(15)
+    l_setSpeedMult(1.7)
+    l_setSpeedInc(0)
+    l_setSpeedMax(3.5)
+    l_setRotationSpeed(0.1)
+    l_setRotationSpeedMax(0.415)
+    l_setRotationSpeedInc(0)
+    l_setDelayMult(1.2)
+    l_setDelayInc(0.0)
+    l_setFastSpin(0.0)
+    l_setSides(6)
+    l_setSidesMin(6)
+    l_setSidesMax(6)
+    l_setIncTime(15)
 
-	l_setPulseMin(77)
-	l_setPulseMax(95)
-	l_setPulseSpeed(1.937)
-	l_setPulseSpeedR(0.524)
-	l_setPulseDelayMax(13.05)
+    l_setPulseMin(77)
+    l_setPulseMax(95)
+    l_setPulseSpeed(1.937)
+    l_setPulseSpeedR(0.524)
+    l_setPulseDelayMax(13.05)
     l_setPulseInitialDelay(28.346) -- skip a beat to match with the clap
 
-	l_setBeatPulseMax(17)
-	l_setBeatPulseDelayMax(28.346)
+    l_setBeatPulseMax(17)
+    l_setBeatPulseDelayMax(28.346)
 
-	l_setSwapEnabled(true)
-	l_setSwapCooldownMult(1.4/u_getSpeedMultDM());
-	l_addTracked("special", "special")
+    l_setSwapEnabled(true)
+    l_setSwapCooldownMult(1.4/u_getSpeedMultDM());
+    l_addTracked("special", "special")
 end
 
 -- onLoad is an hardcoded function that is called when the level is started/restarted
@@ -92,64 +92,64 @@ end
 -- onStep is an hardcoded function that is called when the level timeline is empty
 -- onStep should contain your pattern spawning logic
 function onStep()
-	if (special == "none") then
-		addPattern(keys[index])
-		index = index + 1
-		if index - 1 == #keys then
-			index = 1
-			shuffle(keys)
-		end
-	else
-		addPattern(specialKeys[specialIndex])
-		if (#specialKeys > 1) then
-			specialIndex = specialIndex + 1;
-			
-			if specialIndex - 1 == #specialKeys then
-				specialIndex = 1
-				shuffle(specialKeys)
-			end
-		end
-	end
+    if (special == "none") then
+        addPattern(keys[index])
+        index = index + 1
+        if index - 1 == #keys then
+            index = 1
+            shuffle(keys)
+        end
+    else
+        addPattern(specialKeys[specialIndex])
+        if (#specialKeys > 1) then
+            specialIndex = specialIndex + 1;
+
+            if specialIndex - 1 == #specialKeys then
+                specialIndex = 1
+                shuffle(specialKeys)
+            end
+        end
+    end
 end
 
 local constantFlag = false
 
 -- onIncrement is an hardcoded function that is called when the level difficulty is incremented
 function onIncrement()
-	if special == "none" then
-		special = specials[currSpecial]
-		e_messageAddImportant("Special: "..special, 120)
-		currSpecial = currSpecial + 1
-		if (currSpecial - 1 == #specials) then
-			currSpecial = 1
-			shuffle(specials)
-		end
-		specialIndex = 1
-		if (special == "cage") then
-			specialKeys = {10};
-		elseif (special == "spinner") then
-			specialKeys = {8};
-		elseif (special == "barrage") then
-			specialKeys = {101};
-		elseif (special == "grow") then
-			specialKeys = {13};
-		elseif (special == "swarm") then
-			specialKeys = {102};
-			if (level > 10 and not constantFlag) then
-				e_messageAddImportant("Constant Speed Enabled!", 90)
-				constantFlag = true
-			end
-		end
-		shuffle(specialKeys)
-		l_setSpeedInc(0.1)
-		l_setRotationSpeedInc(0.035)
-	else
-		special = "none"
-		level = level + 1;
-		l_setSpeedInc(0)
-		l_setRotationSpeedInc(0)
-	end
-	l_setSwapCooldownMult(1.7/u_getSpeedMultDM());
+    if special == "none" then
+        special = specials[currSpecial]
+        e_messageAddImportant("Special: "..special, 120)
+        currSpecial = currSpecial + 1
+        if (currSpecial - 1 == #specials) then
+            currSpecial = 1
+            shuffle(specials)
+        end
+        specialIndex = 1
+        if (special == "cage") then
+            specialKeys = {10};
+        elseif (special == "spinner") then
+            specialKeys = {8};
+        elseif (special == "barrage") then
+            specialKeys = {101};
+        elseif (special == "grow") then
+            specialKeys = {13};
+        elseif (special == "swarm") then
+            specialKeys = {102};
+            if (level > 10 and not constantFlag) then
+                e_messageAddImportant("Constant Speed Enabled!", 90)
+                constantFlag = true
+            end
+        end
+        shuffle(specialKeys)
+        l_setSpeedInc(0.1)
+        l_setRotationSpeedInc(0.035)
+    else
+        special = "none"
+        level = level + 1;
+        l_setSpeedInc(0)
+        l_setRotationSpeedInc(0)
+    end
+    l_setSwapCooldownMult(1.7/u_getSpeedMultDM());
 end
 
 -- onUnload is an hardcoded function that is called when the level is closed/restarted
@@ -158,8 +158,8 @@ end
 
 -- onUpdate is an hardcoded function that is called every frame
 function onUpdate(mFrameTime)
-	if not achievementUnlocked and l_getLevelTime() > 90 and u_getDifficultyMult() >= 1 then
-		steam_unlockAchievement("a12_disco")
-		achievementUnlocked = true
-	end
+    if not achievementUnlocked and l_getLevelTime() > 90 and u_getDifficultyMult() >= 1 then
+        steam_unlockAchievement("a12_disco")
+        achievementUnlocked = true
+    end
 end
