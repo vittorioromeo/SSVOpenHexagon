a25ec9b311195fb457da864e43868ffba1fa9abc
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-10-31 18:00:02 +0000

    Add random sandbox testing level

---
+++ _RELEASE/Packs/experimental/Scripts/Levels/sandbox.lua
@@ -0,0 +1,25 @@
+-- onInit is an hardcoded function that is called when the level is first loaded
+function onInit()
+    u_log(u_rndIntUpper(100))
+end
+
+-- onLoad is an hardcoded function that is called when the level is started/restarted
+function onLoad()
+end
+
+-- onStep is an hardcoded function that is called when the level timeline is empty
+-- onStep should contain your pattern spawning logic
+function onStep()
+end
+
+-- onIncrement is an hardcoded function that is called when the level difficulty is incremented
+function onIncrement()
+end
+
+-- onUnload is an hardcoded function that is called when the level is closed/restarted
+function onUnload()
+end
+
+-- onUpdate is an hardcoded function that is called every frame
+function onUpdate(mFrameTime)
+end
