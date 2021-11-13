41d49bfd96e37c0303c063eb5e8548bdf9338ed3
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-11-13 00:39:33 +0000

    Linux stuf

--- buildlx/make_release_client_vbox.sh
+++ buildlx/make_release_client_vbox.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-./make_release_server_vbox_0_cmake.sh
-./make_release_server_vbox_1_build.sh
-./make_release_server_vbox_2_copy.sh
+./make_release_client_vbox_0_cmake.sh
+./make_release_client_vbox_1_build.sh
+./make_release_client_vbox_2_copy.sh
 
