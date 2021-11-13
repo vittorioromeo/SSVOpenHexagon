3a7b87aa1a1c31e2b0991d24040fac60f84838b2
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-11-13 00:39:33 +0000

    Linux stuf

--- buildlx/make_release_client_vbox_2_copy.sh
+++ buildlx/make_release_client_vbox_2_copy.sh
@@ -5,18 +5,80 @@ set -e
 echo ""
 echo ""
 echo "--------------------------------------------------------------------"
-echo "| COPYING TO WINDOWS DRIVE AS 'SSVOpenHexagonVbox'                 |"
+echo "| COPYING TO WINDOWS DRIVE AS 'SSVOpenHexagonLinux'                 |"
 echo "--------------------------------------------------------------------"
 echo ""
 
-cp ./SSVOpenHexagon /media/sf_C_DRIVE/OHWorkspace/SSVOpenHexagon/_RELEASE/SSVOpenHexagonVbox
+cp ./SSVOpenHexagon /media/sf_C_DRIVE/OHWorkspace/SSVOpenHexagon/_RELEASE/SSVOpenHexagonLinux
 
 echo ""
 echo ""
 echo "--------------------------------------------------------------------"
-echo "| COPYING TO VBOX DRIVE AS 'SSVOpenHexagonVbox'                    |"
+echo "| COPYING TO VBOX DRIVE AS 'SSVOpenHexagonLinux'                    |"
 echo "--------------------------------------------------------------------"
 echo ""
 
-cp ./SSVOpenHexagon ../_RELEASE/SSVOpenHexagonVbox
+cp ./SSVOpenHexagon ../_RELEASE/SSVOpenHexagonLinux
+cp ./OHServerControl ../_RELEASE/OHServerControlLinux
+cp ./OHWorkshopUploader ../_RELEASE/OHWorkshopUploaderLinux
 
+echo ""
+echo ""
+echo "--------------------------------------------------------------------"
+echo "| COPYING DEPS TO VBOX DRIVE                                       |"
+echo "--------------------------------------------------------------------"
+echo ""
+
+cp ./_deps/sfml-build/lib/libsfml-audio.so.2.5 ../_RELEASE
+cp ./_deps/sfml-build/lib/libsfml-network.so.2.5 ../_RELEASE
+cp ./_deps/luajit-build/src/libluajit.so ../_RELEASE
+cp ./_deps/zlib-build/libz.so.1 ../_RELEASE
+cp ./_deps/libsodium-cmake-build/libsodium.so ../_RELEASE
+cp ./_deps/imgui-sfml-build/libImGui-SFML.so ../_RELEASE
+cp ./_deps/sfml-build/lib/libsfml-graphics.so.2.5 ../_RELEASE
+cp ./_deps/sfml-build/lib/libsfml-window.so.2.5 ../_RELEASE
+cp ./_deps/sfml-build/lib/libsfml-system.so.2.5 ../_RELEASE
+
+echo ""
+echo ""
+echo "--------------------------------------------------------------------"
+echo "| COPYING SYSTEM DEPS TO VBOX DRIVE                                |"
+echo "--------------------------------------------------------------------"
+echo ""
+
+cp /usr/lib/libdl.so.2 ../_RELEASE
+cp /usr/lib/libXcursor.so.1 ../_RELEASE
+cp /usr/lib/libGL.so.1 ../_RELEASE
+cp /usr/lib/libpthread.so.0 ../_RELEASE
+cp /usr/lib/libstdc++.so.6 ../_RELEASE
+cp /usr/lib/libm.so.6 ../_RELEASE
+cp /usr/lib/libgcc_s.so.1 ../_RELEASE
+cp /usr/lib/libc.so.6 ../_RELEASE
+cp /usr/lib/libopenal.so.1 ../_RELEASE
+cp /usr/lib/libvorbisenc.so.2 ../_RELEASE
+cp /usr/lib/libvorbisfile.so.3 ../_RELEASE
+cp /usr/lib/libvorbis.so.0 ../_RELEASE
+cp /usr/lib/libogg.so.0 ../_RELEASE
+cp /usr/lib/libFLAC.so.8 ../_RELEASE
+cp /usr/lib64/ld-linux-x86-64.so.2 ../_RELEASE
+cp /usr/lib/librt.so.1 ../_RELEASE
+cp /usr/lib/libXrender.so.1 ../_RELEASE
+cp /usr/lib/libXfixes.so.3 ../_RELEASE
+cp /usr/lib/libX11.so.6 ../_RELEASE
+cp /usr/lib/libfreetype.so.6 ../_RELEASE
+cp /usr/lib/libXrandr.so.2 ../_RELEASE
+cp /usr/lib/libudev.so.1 ../_RELEASE
+cp /usr/lib/libGLdispatch.so.0 ../_RELEASE
+cp /usr/lib/libGLX.so.0 ../_RELEASE
+cp /usr/lib/libxcb.so.1 ../_RELEASE
+cp /usr/lib/libbz2.so.1.0 ../_RELEASE
+cp /usr/lib/libpng16.so.16 ../_RELEASE
+cp /usr/lib/libharfbuzz.so.0 ../_RELEASE
+cp /usr/lib/libbrotlidec.so.1 ../_RELEASE
+cp /usr/lib/libXext.so.6 ../_RELEASE
+cp /usr/lib/libXau.so.6 ../_RELEASE
+cp /usr/lib/libXdmcp.so.6 ../_RELEASE
+cp /usr/lib/libgraphite2.so.3 ../_RELEASE
+cp /usr/lib/libglib-2.0.so.0 ../_RELEASE
+cp /usr/lib/libbrotlicommon.so.1 ../_RELEASE
+cp /usr/lib/libpcre.so.1 ../_RELEASE
---
+++ prepare_release_linux.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+rm -Rf ./_PREPARED_RELEASE_LINUX
+rm -Rf ./_PREPARED_RELEASE_LINUX_TEST
+mkdir -p ./_PREPARED_RELEASE_LINUX
+
+cp -r ./_RELEASE/Assets ./_PREPARED_RELEASE_LINUX
+cp -r ./_RELEASE/ConfigOverrides ./_PREPARED_RELEASE_LINUX
+
+mkdir -p ./_PREPARED_RELEASE_LINUX/Packs
+cp -r ./_RELEASE/Packs/base ./_PREPARED_RELEASE_LINUX/Packs
+cp -r ./_RELEASE/Packs/tutorial ./_PREPARED_RELEASE_LINUX/Packs
+cp -r ./_RELEASE/Packs/cube ./_PREPARED_RELEASE_LINUX/Packs
+cp -r ./_RELEASE/Packs/hypercube ./_PREPARED_RELEASE_LINUX/Packs
+cp -r ./_RELEASE/Packs/orthoplex ./_PREPARED_RELEASE_LINUX/Packs
+
+mkdir -p ./_PREPARED_RELEASE_LINUX/Profiles
+
+cp ./_RELEASE/SSVOpenHexagonLinux ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/OHWorkshopUploaderLinux ./_PREPARED_RELEASE_LINUX
+
+cp ./_RELEASE/libImGui-SFML.so ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libdiscord_game_sdk.so ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libluajit.so ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libsdkencryptedappticket.so ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libsfml-audio.so.2.5 ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libsfml-graphics.so.2.5 ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libsfml-network.so.2.5 ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libsfml-system.so.2.5 ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libsfml-window.so.2.5 ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libsodium.so ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libstdc++.so.6 ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libsteam_api.so ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/libz.so.1 ./_PREPARED_RELEASE_LINUX
+
+cp ./_RELEASE/steam_appid.txt ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/windowed.bat ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/windowed_no3D.bat ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/fullscreen.bat ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/fullscreen_no3D.bat ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/highfps.bat ./_PREPARED_RELEASE_LINUX
+cp ./_RELEASE/noaudio.bat ./_PREPARED_RELEASE_LINUX
+
+cp ./_RELEASE/run_ssvopenhexagon_linux.sh ./_PREPARED_RELEASE_LINUX
+
+cd ./_PREPARED_RELEASE_LINUX
+chmod +x ./*.so
+# upx -9 ./*.so
+# upx -9 ./SSVOpenHexagonLinux
+# upx -9 ./OHWorkshopUploaderLinux
+cd ..
+
+cp -r ./_PREPARED_RELEASE_LINUX ./_PREPARED_RELEASE_LINUX_TEST
+
+rm -Rf /media/sf_C_DRIVE/OHWorkspace/SSVOpenHexagon/_PREPARED_RELEASE_LINUX
+cp -r ./_PREPARED_RELEASE_LINUX /media/sf_C_DRIVE/OHWorkspace/SSVOpenHexagon/
