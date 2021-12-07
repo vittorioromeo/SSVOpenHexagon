93d6d6a89134842f8e52eeee9ba09ca7d6d46d9a
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-12-07 18:47:48 +0000

    Update to SFML 3.x

--- buildrel/copylibs.sh
+++ buildrel/copylibs.sh
@@ -3,23 +3,23 @@
 cp ./_deps/imgui-sfml-build/libImGui-SFML_d.dll ../_RELEASE/ &
 cp ./_deps/imgui-sfml-build/libImGui-SFML.dll ../_RELEASE/ &
 
-cp ./_deps/sfml-build/lib/libsfml-graphics-d-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/libsfml-system-d-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/libsfml-window-d-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/libsfml-network-d-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/libsfml-audio-d-2.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/libsfml-graphics-d-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/libsfml-system-d-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/libsfml-window-d-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/libsfml-network-d-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/libsfml-audio-d-3.dll ../_RELEASE/ &
 
-cp ./_deps/sfml-build/lib/sfml-graphics-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/sfml-system-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/sfml-window-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/sfml-network-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/sfml-audio-2.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/sfml-graphics-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/sfml-system-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/sfml-window-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/sfml-network-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/sfml-audio-3.dll ../_RELEASE/ &
 
-cp ./_deps/sfml-build/lib/libsfml-graphics-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/libsfml-system-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/libsfml-window-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/libsfml-network-2.dll ../_RELEASE/ &
-cp ./_deps/sfml-build/lib/libsfml-audio-2.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/libsfml-graphics-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/libsfml-system-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/libsfml-window-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/libsfml-network-3.dll ../_RELEASE/ &
+cp ./_deps/sfml-build/lib/libsfml-audio-3.dll ../_RELEASE/ &
 
 cp ./_deps/sfml-src/extlibs/bin/x64/openal32.dll ../_RELEASE/ &
 cp ./_deps/libsodium-cmake-build/libsodium.dll ../_RELEASE/ &
@@ -29,23 +29,23 @@ cp ./_deps/zlib-build/libzlib1.dll ../_RELEASE/ &
 cp ./_deps/imgui-sfml-build/libImGui-SFML_d.dll ./test/ &
 cp ./_deps/imgui-sfml-build/libImGui-SFML.dll ./test/ &
 
-cp ./_deps/sfml-build/lib/libsfml-graphics-d-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/libsfml-system-d-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/libsfml-window-d-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/libsfml-network-d-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/libsfml-audio-d-2.dll ./test/ &
+cp ./_deps/sfml-build/lib/libsfml-graphics-d-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/libsfml-system-d-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/libsfml-window-d-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/libsfml-network-d-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/libsfml-audio-d-3.dll ./test/ &
 
-cp ./_deps/sfml-build/lib/sfml-graphics-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/sfml-system-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/sfml-window-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/sfml-network-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/sfml-audio-2.dll ./test/ &
+cp ./_deps/sfml-build/lib/sfml-graphics-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/sfml-system-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/sfml-window-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/sfml-network-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/sfml-audio-3.dll ./test/ &
 
-cp ./_deps/sfml-build/lib/libsfml-graphics-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/libsfml-system-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/libsfml-window-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/libsfml-network-2.dll ./test/ &
-cp ./_deps/sfml-build/lib/libsfml-audio-2.dll ./test/ &
+cp ./_deps/sfml-build/lib/libsfml-graphics-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/libsfml-system-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/libsfml-window-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/libsfml-network-3.dll ./test/ &
+cp ./_deps/sfml-build/lib/libsfml-audio-3.dll ./test/ &
 
 cp ./_deps/sfml-src/extlibs/bin/x64/openal32.dll ./test/ &
 cp ./_deps/libsodium-cmake-build/libsodium.dll ./test/ &
