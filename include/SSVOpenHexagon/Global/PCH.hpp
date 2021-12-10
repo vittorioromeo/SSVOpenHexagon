eb1a5c456385ff8f7a5b9670aac0f3e54af4ee6e
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-12-10 13:33:41 +0000

    PCH fixes

--- include/SSVOpenHexagon/Global/PCH.hpp
+++ include/SSVOpenHexagon/Global/PCH.hpp
@@ -5,8 +5,17 @@
 #pragma once
 
 #ifdef _WIN32
+
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
 #include <windows.h>
+
 #endif
 
 #include <algorithm>
