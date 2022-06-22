7b25eec275c88efd0384f0c1757e91c74c77f24b
Vittorio Romeo <vittorio.romeo@outlook.com>
2022-06-22 13:51:37 +0200

    Update external libs, improve build scripts

--- buildrel/make_release_client_win10_msys_0_cmake.sh
+++ buildrel/make_release_client_win10_msys_0_cmake.sh
@@ -9,16 +9,31 @@ echo "| RUNNING CMAKE IN RELEASE MODE (NINJA)                            |"
 echo "--------------------------------------------------------------------"
 echo ""
 
+# cmake .. -G"Ninja" \
+#          -DFORCE_COLORED_OUTPUT=1 \
+#          -DCMAKE_BUILD_TYPE=RELEASE \
+#          -DCMAKE_C_COMPILER="gcc" \
+#          -DCMAKE_C_FLAGS="-fuse-ld=lld" \
+#          -DCMAKE_CXX_COMPILER="g++" \
+#          -DCMAKE_CXX_FLAGS="\
+#              -fuse-ld=lld \
+#              -Wall -Wextra -Wpedantic -Wno-braced-scalar-init \
+#              -Wno-pragmas -Wno-missing-field-initializers \
+#              -Wno-stringop-overflow \
+#              -O3 -DNDEBUG \
+#              -frounding-math -fsignaling-nans -ffloat-store -ffp-contract=off \
+#              -g3" # TODO (P0): for profiling, remove
+
 cmake .. -G"Ninja" \
          -DFORCE_COLORED_OUTPUT=1 \
          -DCMAKE_BUILD_TYPE=RELEASE \
-         -DCMAKE_C_COMPILER="gcc" \
+         -DCMAKE_C_COMPILER="clang" \
          -DCMAKE_C_FLAGS="-fuse-ld=lld" \
-         -DCMAKE_CXX_COMPILER="g++" \
+         -DCMAKE_CXX_COMPILER="clang++" \
          -DCMAKE_CXX_FLAGS="\
              -fuse-ld=lld \
              -Wall -Wextra -Wpedantic -Wno-braced-scalar-init \
              -Wno-pragmas -Wno-missing-field-initializers \
              -O3 -DNDEBUG \
-             -frounding-math -fsignaling-nans -ffloat-store -ffp-contract=off\
+             -frounding-math -ffp-contract=off \
              -g3" # TODO (P0): for profiling, remove
