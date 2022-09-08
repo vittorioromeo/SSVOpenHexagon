fd9054d07128f7ed6b877e54710b75003dd1350a
Bauumm <oh4@web.de>
2022-09-08 22:35:41 +0200

    Add onPreUnload

--- src/SSVOpenHexagon/Core/HexagonGame.cpp
+++ src/SSVOpenHexagon/Core/HexagonGame.cpp
@@ -700,6 +700,7 @@ void HexagonGame::newGame(const std::string& mPackId, const std::string& mId,
     inputImplCCW = inputImplCW = false;
     playerNowReadyToSwap = false;
 
+    runLuaFunctionIfExists<void>("onPreUnload");
     lua = Lua::LuaContext{};
     calledDeprecatedFunctions.clear();
     initLua();
