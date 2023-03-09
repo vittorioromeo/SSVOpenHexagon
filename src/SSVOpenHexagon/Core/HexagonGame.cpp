aae19952b61ec213495b7af2394b01b8cfe76d07
Vittorio Romeo <vittorio.romeo@outlook.com>
2023-03-09 14:33:06 +0100

    Fix broken "replay restart feeature"

--- src/SSVOpenHexagon/Core/HexagonGame.cpp
+++ src/SSVOpenHexagon/Core/HexagonGame.cpp
@@ -384,7 +384,7 @@ HexagonGame::HexagonGame(Steam::steam_manager* mSteamManager,
         notInConsole(
             [this]
             {
-                if((deathInputIgnore <= 0.f && status.hasDied) || inReplay())
+                if(deathInputIgnore <= 0.f && status.hasDied)
                 {
                     status.mustStateChange = StateChange::MustRestart;
                 }
@@ -394,7 +394,7 @@ HexagonGame::HexagonGame(Steam::steam_manager* mSteamManager,
         notInConsole(
             [this]
             {
-                if(deathInputIgnore <= 0.f && status.hasDied)
+                if((deathInputIgnore <= 0.f && status.hasDied) || inReplay())
                 {
                     status.mustStateChange = StateChange::MustReplay;
                 }
