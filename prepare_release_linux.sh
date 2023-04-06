b21510833553f326da9c475c4f0dfde0b20a2c9b
Vittorio Romeo <vittorio.romeo@outlook.com>
2023-04-07 00:15:46 +0200

    WIP

--- prepare_release_linux.sh
+++ prepare_release_linux.sh
@@ -25,6 +25,7 @@ cp ./_RELEASE/libsteam_api.so ./_PREPARED_RELEASE_LINUX
 cp ./_RELEASE/libz.so.1 ./_PREPARED_RELEASE_LINUX
 cp /lib/x86_64-linux-gnu/libopenal.so.1 ./_PREPARED_RELEASE_LINUX
 cp /lib/x86_64-linux-gnu/libFLAC.so.8 ./_PREPARED_RELEASE_LINUX
+cp /lib/x86_64-linux-gnu/libsndio.so.7 ./_PREPARED_RELEASE_LINUX
 
 cp ./_RELEASE/steam_appid.txt ./_PREPARED_RELEASE_LINUX
 
@@ -36,17 +37,17 @@ chmod +x ./*.so
 chmod +x ./SSVOpenHexagonLinux
 chmod +x ./OHWorkshopUploaderLinux
 
-echo "PACKING WITH UPX"
-
-for x in ./*.so; do
-    upx -9 $x &
-done
-
-upx -9 ./SSVOpenHexagonLinux &
-upx -9 ./OHWorkshopUploaderLinux &
-
-wait
-echo "DONE PACKING WITH UPX"
+# echo "PACKING WITH UPX"
+#
+# for x in ./*.so; do
+#     upx -9 $x &
+# done
+#
+# upx -9 ./SSVOpenHexagonLinux &
+# upx -9 ./OHWorkshopUploaderLinux &
+#
+# wait
+# echo "DONE PACKING WITH UPX"
 
 cd ..
 
